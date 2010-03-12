c962fc31bb741e2576871ef8fd140ac063913086
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-03-12 00:22:47 +0000

    the file implements all the errors that are raised in openamos

---
+++ trunk/openamos/core/errors.py
@@ -0,0 +1,27 @@
+class SpecificationError(Exception):
+    pass
+
+class ChoicesError(SpecificationError):
+    pass
+
+class VariablesError(SpecificationError):
+    pass
+
+class CoefficientsError(SpecificationError):
+    pass
+
+class SeedError(SpecificationError):
+    pass
+
+class ThresholdsError(SpecificationError):
+    pass
+
+class DataError(Exception):
+    pass
+
+class ProbabilityError(Exception):
+    pass
+
+class ErrorSpecificationError(Exception):
+    pass
+
