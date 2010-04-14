28cd2cc7ed9b6bdf74abc5f30f88fbc2083aa47b
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-04-14 16:13:25 +0000

    additional DataFilter class added that is used to specifiy filters

--- trunk/openamos/core/data_array.py
+++ trunk/openamos/core/data_array.py
@@ -76,6 +76,7 @@ class DataArray(object):
         return repr(self.data)
 
     def column(self, columname):
+        self.check_varname(columname)
         if not isinstance(columname, str):
             raise DataError, 'not a valid column name'
         try:
@@ -85,6 +86,7 @@ class DataArray(object):
             raise DataError, 'not a recognized column name'
 
     def setcolumn(self, columname, values, rows=None):
+        self.check_varname(columname)
         colnum = self._colnames[columname.lower()]
         try:
             if rows is None:
@@ -95,12 +97,14 @@ class DataArray(object):
             raise DataError, e
         
     def scaledowncolumn(self, columname, scale):
+        self.check_varname(columname)
         if type(scale) not in [int, float]:
             raise DataError, 'the scale values is not a valid number'
         colnum = self._colnames[columname.lower()]
         self.data[:,colnum] = self.data[:,colnum]/scale
 
     def addtocolumn(self, columname, values):
+        self.check_varname(columname)
         colnum = self._colnames[columname.lower()]
         try:
             self.data[:,colnum] = self.data[:,colnum] + values
@@ -108,11 +112,122 @@ class DataArray(object):
             raise DataError, e
 
     def expofcolumn(self, columname):
+        self.check_varname(columname)
         colnum = self._colnames[columname.lower()]
         try:
             self.data[:,colnum] = exp(self.data[:,colnum])
         except ValueError, e:
             raise DataError, e
+
+
+    def columns(self, columnames):
+        """
+        the method retrieves and returns a dataarray of columnames that 
+        were passed to the method. 
+        """
+        if not isinstance(columnames, list):
+            return DataError, """the column names input should be a list"""\
+                """ of variable names"""
+        
+        for i in columnames:
+            self.check_varname(i)
+
+        columnums = []
+        for i in columnames:
+            try:
+                columnums.append(self._colnames[i.lower()])
+            except KeyError, e:
+                raise DataError, '%s not a recognized column name' %i
+
+        return DataArray(self.data[:,columnums], columnames)
+
+    def rowsof(self, rows):
+        """
+        the method retrieves and returns a dataarray of rows that 
+        were passed to the method.
+        """
+        
+        if isinstance(rows, ndarray):
+            if rows.size <> self.rows:
+                raise DataError, """the number of rows is inconsistent with """\
+                    """the number of rows in the data"""
+            if len(rows.shape) > 1:
+                rows = array([i[0] for i in rows])
+
+        rows_type = type(rows)
+        if rows_type in [tuple, list]:
+            if len(rows) <> self.rows:
+                raise DataError, """the number of rows is inconsistent with """\
+                    """the number of rows in the data"""
+            rows = array(rows)
+        
+        return DataArray(self.data[rows,:], self.varnames)
+
+
+    def __lt__(self, value):
+        return self.data < value
+
+    def __gt__(self, value):
+        return self.data > value
+
+    def __le__(self, value):
+        return self.data <= value
+
+    def __ge__(self, value):
+        return self.data >= value
+
+    def __eq__(self, value):
+        return self.data == value
+
+    def __ne__(self, value):
+        return self.data <> value
+
+
+class DataFilter(object):
+    def __init__(self, varname, filter_string, value, coefficients):
+        
+        if not isinstance(varname, str):
+            raise DataError, """variable input has to be a valid """\
+                """ string object"""
+        self.varname = varname
+        
+        if not isinstance(filter_string, str):
+            raise DataError, """the data filter string has to be a valid """\
+                """ string object"""
+
+        value_type = type(value)
+
+        if not value_type in [int, float]:
+            raise DataError, """the value has to be a valid numeric object; """\
+                """only 'float' and 'int' objects are accepted"""
+        self.value = value
+
+
+        if not isinstance(coefficients, dict):
+            raise DataError, 'coefficient input is invalid - should be of dictionary type'
+
+        self.coefficients = coefficients
+
+    def compare(self, data):
+        if self.filter_string == 'less than':
+            valid_rows = data.columns([self.varname]) < self.value
+            
+        if self.filter_string == 'greater than':
+            valid_rows = data.columns([self.varname]) > self.value
+
+        if self.filter_string == 'less than equals':
+            valid_rows = data.columns([self.varname]) <= self.value
+
+        if self.filter_string == 'greater than equals':
+            valid_rows = data.columns([self.varname]) >= self.value
+
+        if self.filter_string == 'equals':
+            valid_rows = data.columns([self.varname]) == self.value
+
+        if self.filter_string == 'not equals':
+            valid_rows = data.columns([self.varname]) <> self.value
+
+        return data.rowsof(valid_rows)            
 
 
 import unittest
@@ -156,7 +271,6 @@ class TestDataArray(unittest.TestCase):
         self.coefficients = {'constant':2.0, 'var1':1.5}
 
         
-
     def testdata(self):
         data_array = DataArray(self.data1, self.varnames)
         self.assertEqual(type(self.data), type(data_array.data))
@@ -173,7 +287,20 @@ class TestDataArray(unittest.TestCase):
         result_exp_actual = exp(result_actual)
         diff_result = any(result_exp_actual == result_exp)
         self.assertEqual(True, diff_result)
-         
+
+    def testretrievecolumns(self):
+        data_array = DataArray(self.data1, self.varnames)
+        columnames = ['Var1', 'constant']
+        result = data_array.columns(columnames)
+
+        columnames_lower = [i.lower() for i in columnames]
+        self.assertEqual(columnames_lower, result.varnames)
+        
+        rows = data_array.columns(['constant']) == 2
+        result = data_array.rowsof(rows)
+        print result.varnames
+        print result.data.shape[0]
+
         
 if __name__ == '__main__':
     unittest.main()
