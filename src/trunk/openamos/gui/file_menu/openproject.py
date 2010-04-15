0ebf1f4891ab6efb1979212c7063e51bfed84a52
bhargavakishore.sana <bhargavakishore.sana@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-04-15 23:54:31 +0000

    Change made to pick up an project xml file

--- src/trunk/openamos/gui/file_menu/openproject.py
+++ src/trunk/openamos/gui/file_menu/openproject.py
@@ -3,13 +3,8 @@ from PyQt4.QtCore import *
 
 
 
-class OpenProject(QWizard):
+class OpenProject(QFileDialog):
     def __init__(self, parent = None):
         super(OpenProject, self).__init__(parent)
-        self.setWindowTitle("Open a peoject")
-        self.showdialog()
-
-    def showdialog(self):
-        filename = QFileDialog.getOpenFileName(self, 'Open file',
-                    '/home')
-        file=open(filename)
+        self.file = self.getOpenFileName(parent, "Browse to select a project configuration file", "/home",
+                                         "XML File (*.xml)")
