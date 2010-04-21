f9530dd90f4981e9d390d932fd89d2412cd4ed03
bapat.namrata <bapat.namrata@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-04-21 17:10:55 +0000

    unit test added

--- src/trunk/openamos/core/database_management/deleteDB.py
+++ src/trunk/openamos/core/database_management/deleteDB.py
@@ -6,6 +6,7 @@ import exceptions
 import sqlalchemy
 from sqlalchemy import create_engine
 from newdb import connectDB
+from dispdb import displayDB
 
 #class to insert data into the database
 class deleteDB:
@@ -16,12 +17,6 @@ class deleteDB:
 		self.dbobj = dbobj
 		self.result = result
 
-		#print the data
-		print self.table_name
-		print self.value
-		print self.dbobj
-		print self.result
-
 		try:	
 			#created an instance of the database object to connect to it		
 			self.dbobj = connectDB('localhost', 'postgres', '1234', 'postgres', 'engine', 'connection')
@@ -29,24 +24,45 @@ class deleteDB:
 			#try to execute simple insert query
 			self.result = self.dbobj.connection.execute("delete from %s where age = %s" % (self.table_name, self.value))
 			
-			#after inserting the data, print it
-			self.result = self.dbobj.connection.execute("select * from %s" %(self.table_name))
-			print self.result.fetchall()
-			#self.result = self.db.connection.execute("select table_name from information_schema.tables "\
-			#					 "where table_schema = 'public'")
-			#print self.result.fetchall()
+			#after deleting the data, print it
+			disp_obj = displayDB(self.dbobj, self.table_name, '', '', self.result)
+
 		
 		except Exception, e:
 		        #Exception occured. print the error
 		        print "Error: %s" %e
 			print "Error while inserting and displaying data from the database"
 
-		
+		#close the database connection
+		print "Close the connection"
+		self.dbobj.conn_close(self.dbobj.connection)
+
+
+#unit test to test the code
+import unittest
+
+#define a class for testing
+class TestDeleteDB(unittest.TestCase):
+	#only initialize objects here
+	def setUp(self):
+		self.dbobj = 'abc'
+		self.table_name = 'users'
+		self.result = 'xzy'
+		self.value = '10'
+
+
+	def testDB(self):
+		#test to insert and then display rows
+		DBobj = deleteDB(self.dbobj, self.table_name, self.result, self.value)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 #creating an instance of the class.
-dispInstance = deleteDB('dbobj', 'users', 'result', '8')
+#dispInstance = deleteDB('dbobj', 'users', 'result', '8')
 #dispInstance = displayDB()
 
-#have to add more function to drop table and delete table.
 		
 
