07397f0b8b6b55af475388a081b2dd56c4997ea1
bapat.namrata <bapat.namrata@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-05-24 14:27:39 +0000

    Indentation changed

--- src/trunk/openamos/core/database_management/database_configuration.py
+++ src/trunk/openamos/core/database_management/database_configuration.py
@@ -5,22 +5,24 @@ import os
 #from database_connection import DBConnection
 
 class DataBaseConfiguration(object):
-	"""Database configuration object is the structure of the database.
-	The object describes the attributes to connect to the database.
-	for now using the __init__ method to define the object"""
+    """
+    Database configuration object is the structure of the database.
+    The object describes the attributes to connect to the database.
+    for now using the __init__ method to define the object
+    """
 	
-	def __init__(self,
-	             protocol = None,
-		     user_name = None,
-		     password = None,
-                     host_name = None,
-		     database_name = None):
+    def __init__(self,
+                protocol = None,
+                user_name = None,
+                password = None,
+                host_name = None,
+                database_name = None):
 
-		self.protocol = protocol
-		self.user_name = user_name
-		self.password = password
-		self.host_name = host_name
-		self.database_name = database_name
+        self.protocol = protocol
+        self.user_name = user_name
+        self.password = password
+        self.host_name = host_name
+        self.database_name = database_name
 
 		#DBConnection.__init__(self,
 		#		protocol = protocol,
@@ -32,12 +34,10 @@ class DataBaseConfiguration(object):
 		#self.database_name = database_name
 
 
-	def __repr__(self):
-		return '%s://%s:%s@%s:5432/%s'%(self.protocol, self.user_name,
-						self.password, self.host_name,
-						self.database_name)
-
-
+    def __repr__(self):
+        return '%s://%s:%s@%s:5432/%s'%(self.protocol, self.user_name,
+                                        self.password, self.host_name,
+                                        self.database_name)
 
 
 #unit test to test the code
@@ -45,25 +45,19 @@ import unittest
 
 #define a class for testing
 class TestDBConfiguration(unittest.TestCase):
-	#only initialize objects here
-	def setUp(self):
-		self.protocol = 'postgres'		
-		self.user_name = 'postgres'
-		self.password = '1234'
-		self.host_name = 'localhost'
-		self.database_name = 'test'
+    #only initialize objects here
+    def setUp(self):
+        self.protocol = 'postgres'		
+        self.user_name = 'postgres'
+        self.password = '1234'
+        self.host_name = 'localhost'
+        self.database_name = 'test'
 
-
-	def testDB(self):
-		#test to connect to database 
-		DBobj = DataBaseConfiguration(self.protocol, self.user_name, self.password, self.host_name, self.database_name)
-		print DBobj
+    def testDB(self):
+        #test to connect to database 
+        DBobj = DataBaseConfiguration(self.protocol, self.user_name, self.password, self.host_name, self.database_name)
+        print DBobj
 
 
 if __name__ == '__main__':
-    unittest.main()
-
-
-
-
-	
+unittest.main()
