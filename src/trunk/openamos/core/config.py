a776c166a6c34d616e649a228e18c98224791de8
bhargavakishore.sana <bhargavakishore.sana@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-07-06 18:27:15 +0000

    config object containing project properties and model specifications

--- src/trunk/openamos/core/config.py
+++ src/trunk/openamos/core/config.py
@@ -6,6 +6,10 @@ Created on Jun 8, 2010
 
 from lxml import etree
 
+from openamos.gui.env import *
+import os
+from copy import deepcopy
+
 class ConfigObject(object):
     '''
     classdocs
@@ -13,7 +17,8 @@ class ConfigObject(object):
 
     def __init__(self,configfileloc = None, configtree = None):
         if configfileloc:
-            self.protree = etree.parse(configfileloc)
+            parser = etree.XMLParser(remove_blank_text=True)
+            self.protree = etree.parse(configfileloc,parser)
         elif configtree:
             self.protree = configtree        
 
@@ -22,4 +27,61 @@ class ConfigObject(object):
         if prop == 'text':
             return self.protree.findtext(elt)
         else:
-            return element.get(prop)        
+            return element.get(prop)  
+
+    def modelSpecInConfig(self,modelkey):
+        compname = self.getCompName(modelkey)
+        modelconfigelt = self.protree.find(MODELCONFIG)
+        if modelconfigelt != None:
+            modelfound = None
+            for comp in modelconfigelt.getiterator(COMP):
+                if compname == comp.get(NAME):
+                    for model in comp.getiterator(MODEL): 
+                        if modelkey == model.get(NAME):
+                            modelfound = deepcopy(model)
+            return modelfound
+        else:
+            return None
+    
+    def addModelElement(self,modelelt):
+        compname = self.getCompName(modelelt.get(NAME))
+        modelconfigelt = self.protree.find(MODELCONFIG)
+        if modelconfigelt != None:
+            compfound = False
+            for comp in modelconfigelt.getiterator(COMP):
+                if compname == comp.get(NAME):
+                    compfound = True
+                    modelidx = 0
+                    modelreplaced = False
+                    for model in comp.getiterator(MODEL):
+                        if modelelt.get(NAME) == model.get(NAME):
+                            comp.remove(model)
+                            comp.insert(modelidx,modelelt)
+                            modelreplaced = True
+                        modelidx = modelidx + 1
+                    if not modelreplaced:
+                        comp.append(modelelt)
+            if not compfound:
+                compelt = etree.SubElement(modelconfigelt,COMP)
+                compelt.set(NAME,compname)
+                compelt.append(modelelt) 
+        else:
+            proconfigelt = self.protree.getroot()  
+            modelconfigelt = etree.SubElement(proconfigelt,MODELCONFIG) 
+            compelt = etree.SubElement(modelconfigelt,COMP)
+            compelt.set(NAME,compname)
+            compelt.append(modelelt)  
+    
+    def getCompName(self,modelkey):
+        for compkey in COMPMODELMAP.keys():
+            modelist = COMPMODELMAP[compkey]
+            if modelkey in modelist:
+                return compkey
+    
+    def write(self):
+        projecthome = self.getConfigElement(PROJECT_HOME)
+        projectname = self.getConfigElement(PROJECT_NAME)
+        configfileloc = projecthome + os.path.sep + projectname + '.xml'
+        configfile = open(configfileloc, 'w')
+        self.protree.write(configfile, pretty_print=True)
+        configfile.close()
