88de36ea48e3184294c0530f1cbd047971caf71e
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-08-24 20:17:02 +0000

    Connect spec_abstract_dialog to fixed_activity_models.py

--- src/trunk/openamos/gui/model_manager/fixed_activity_models.py
+++ src/trunk/openamos/gui/model_manager/fixed_activity_models.py
@@ -1,13 +1,13 @@
 # -*- coding: cp936 -*-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
+from spec_abstract_dialog import *
 from openamos.gui.env import *
 
 
 
 class FixedActivityModels(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, co=None):
         super(FixedActivityModels, self).__init__(parent)
         
         self.setWindowTitle('Fixed Activity Location Choice Generator')
@@ -32,18 +32,49 @@ class FixedActivityModels(QWidget):
 
         work_loc_button = QPushButton('For each job identify \na primary work location;\n\n\nDuring simulation, \ngenerate Choice Sets \nconditional on the home \nlocation e.g. within a 90 \nmin or 120 min travel time\n\nWork related trips/\nactivities will be generated \nduring the open periods to \naccount for multiple job \nsites per job', self)
         work_loc_button.setGeometry((size.width()) / 4 - 100, size.height() / 2 - 200, 200, 400)
+        self.connect(work_loc_button, SIGNAL('clicked()'), self.work_loc)
 
 
         school_loc_button = QPushButton('School location choice\n\nDuring simulation, \ngenerate Choice Sets \nconditional on the home \nlocation e.g. within a 60 \nmin', self)
         school_loc_button.setGeometry((size.width()) / 2 - 100, size.height() / 2 - 200, 200, 400)
+        self.connect(school_loc_button, SIGNAL('clicked()'), self.school_loc)
 
 
         preschool_loc_button = QPushButton('Pre-school location \nchoice\n\nDuring simulation, \ngenerate Choice Sets \nthat are within a 20 \nminute driving distance \nfrom home or work \nlocation of the adult(s)\n\nAlternatively just identify\n possible locations similar \nto school location', self)
         preschool_loc_button.setGeometry((size.width()) * 3 / 4 - 100, size.height() / 2 - 200, 200, 400)
+        self.connect(preschool_loc_button, SIGNAL('clicked()'), self.preschool_loc)
 
 
         activity_loc_button = QPushButton('Fixed activity locations for all individuals within the population', self)
         activity_loc_button.setGeometry((size.width()) / 4 - 100, size.height() / 2 + 230, (size.width()) / 2 + 200, 50)
+        
+        self.configob = co
+        
+        
+    def work_loc(self):
+        diagtitle = COMPMODEL_WORKLOC
+        modelkey = MODELKEY_WORKLOC
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+        
+    def school_loc(self):
+        diagtitle = COMPMODEL_SCHLOC1
+        modelkey = MODELKEY_SCHLOC1
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+        
+    def preschool_loc(self):
+        diagtitle = COMPMODEL_PRESCHLOC
+        modelkey = MODELKEY_PRESCHLOC
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+                
+        
 
     def paintEvent(self, parent = None):
         # Drawing line
