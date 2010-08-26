5fcdf81ff638e65bc3473b745c7e56db80f5a7a4
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-08-26 16:31:20 +0000

    Connect spec_abstract_dialog.py to activity_travel_pattern_simulator. Therefore, users can open dialog by clicking a button on a diagram.

--- src/trunk/openamos/gui/model_manager/activity_travel_pattern_simulator.py
+++ src/trunk/openamos/gui/model_manager/activity_travel_pattern_simulator.py
@@ -1,8 +1,10 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from spec_abstract_dialog import *
+from openamos.gui.env import *
 
 class Activity_Travel_Pattern_Simulator(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, co=None):
         super(Activity_Travel_Pattern_Simulator, self).__init__(parent)
         self.setWindowTitle('Activity Travel Pattern Simulator')
         self.setAutoFillBackground(True)
@@ -15,71 +17,223 @@ class Activity_Travel_Pattern_Simulator(QWidget):
         
         time_slice_button = QPushButton('Within a time slice', self)
         time_slice_button.setGeometry(size.width() / 2 - 100, 40, 200, 50)
+        self.connect(time_slice_button, SIGNAL('clicked()'), self.time_slice)
 
         children_button = QPushButton('Children with after school \ndependent activities', self)
         children_button.setGeometry((size.width()) * 3 / 4 - 100, 120, 200, 50)
 
         activity_pursued_1_button = QPushButton('Can activity be pursued \njointly (Spatial and Temporal \nfeasibility) with a Household \nmember?', self)
         activity_pursued_1_button.setGeometry((size.width()) * 3 / 4 - 100, 200, 200, 70)
+        self.connect(activity_pursued_1_button, SIGNAL('clicked()'), self.activity_pursued_1)
 
         assign_to_non_hhold_button = QPushButton('Activity assigned to a \nNon-household member \ncomprising Joint Activity with \nNon-household member', self)
         assign_to_non_hhold_button.setGeometry((size.width()) * 3 / 4 + 40, 300, 220, 70)
+        self.connect(assign_to_non_hhold_button, SIGNAL('clicked()'), self.assign_to_non_hhold)
 
         assign_to_hhold_button = QPushButton('Assign the activity to \nHousehold member comprising\nJoint Activity with \nhousehold member', self)
         assign_to_hhold_button.setGeometry((size.width()) * 3 / 4 - 260, 300, 220, 70)
+        self.connect(assign_to_hhold_button, SIGNAL('clicked()'), self.assign_to_hhold)
 
         mode_choice_model_button = QPushButton('Mode choice model for \nintra-household joint \ntrips with children', self)
         mode_choice_model_button.setGeometry((size.width()) * 3 / 4 - 260, 400, 220, 50)
-
-
-
+        self.connect(mode_choice_model_button, SIGNAL('clicked()'), self.mode_choice_model)
        
         all_other_individuals_button = QPushButton('All other individuals', self)
         all_other_individuals_button.setGeometry((size.width()) / 4 - 110, 120, 220, 50)
 
         adult_individuals_button = QPushButton('Adult individuals, children \nwith independent activities \n(if children stay at home then \nthey shadow the activity-travel \npatterns of adult to whom \nthey are assigned)', self)
-        adult_individuals_button.setGeometry((size.width()) / 4 - 110, 200, 220, 100)       
+        adult_individuals_button.setGeometry((size.width()) / 4 - 110, 200, 220, 100)
+        self.connect(adult_individuals_button, SIGNAL('clicked()'), self.adult_individuals)       
 
         travel_time_button = QPushButton('Is travel time to next \nfixed activity \74 time \navailable in the prism?', self)
         travel_time_button.setGeometry((size.width()) / 4 - 110, 330, 220, 50)
+        self.connect(travel_time_button, SIGNAL('clicked()'), self.travel_time)
 
         activity_choice_button = QPushButton('Activity Type Choice\nMode-Destination Choice; mode-\ndestination choices are limited \nby the TSP and travel time to \nnext fixed activity\nActivity Duration Choice', self)
         activity_choice_button.setGeometry((size.width()) / 4 - 260, 410, 220, 100)
+        self.connect(activity_choice_button, SIGNAL('clicked()'), self.activity_choice)
 
         actual_start_time_button = QPushButton('Actual start time \nfor the activity', self)
         actual_start_time_button.setGeometry((size.width()) / 4 - 260, 540, 220, 50)
+        self.connect(actual_start_time_button, SIGNAL('clicked()'), self.actual_start_time)
 
         time_in_activity_button = QPushButton('Is there enough time to \nengage in the activity?', self)
         time_in_activity_button.setGeometry((size.width()) / 4 - 260, 620, 220, 50)
+        self.connect(time_in_activity_button, SIGNAL('clicked()'), self.time_in_activity)
 
         proceed_next_activity_button = QPushButton('Proceed to next \nfixed activity', self)
         proceed_next_activity_button.setGeometry((size.width()) / 4 + 40, 620, 220, 50)
+        self.connect(proceed_next_activity_button, SIGNAL('clicked()'), self.proceed_next_activity)
 
         mode_choice_next_activity_button = QPushButton('Mode Choice to the \nnext fixed activity', self)
         mode_choice_next_activity_button.setGeometry((size.width()) / 4 + 40, 700, 220, 50)
+        self.connect(mode_choice_next_activity_button, SIGNAL('clicked()'), self.mode_choice_next_activity)
 
         hov_button = QPushButton('Is the mode of \nthe trip HOV?', self)
         hov_button.setGeometry((size.width()) / 4 - 250, 790, 200, 50)
+        self.connect(hov_button, SIGNAL('clicked()'), self.hov)
 
         activity_pursued_2_button = QPushButton('Can activity be pursued \njointly (Spatial and Temporal \nfeasibility) with Household \nmembers?', self)
         activity_pursued_2_button.setGeometry((size.width()) / 4 + 50, 780, 200, 70)
+        self.connect(activity_pursued_2_button, SIGNAL('clicked()'), self.activity_pursued_2)
 
         check_if_join_activity_button = QPushButton('For each available household \nmember, check to see if \nhe/she will join the activity?', self)
         check_if_join_activity_button.setGeometry((size.width()) / 4 + 350, 780, 200, 70)
+        self.connect(check_if_join_activity_button, SIGNAL('clicked()'), self.check_if_join_activity)
 
         activity_non_hhold_button = QPushButton('Joint Activity with \nNon-household member', self)
         activity_non_hhold_button.setGeometry((size.width()) / 4 + 50, 880, 200, 50)
+        self.connect(activity_non_hhold_button, SIGNAL('clicked()'), self.activity_non_hhold)
 
         activity_hhold_button = QPushButton('Joint Activity with \nhousehold member', self)
         activity_hhold_button.setGeometry((size.width()) / 4 + 350, 880, 200, 50)
+        self.connect(activity_hhold_button, SIGNAL('clicked()'), self.activity_hhold)
 
         sov_hov_button = QPushButton('If mode is SOV or HOV Driver identify \nvehicle (for only trips where household \nmember is the driver) ', self)
         sov_hov_button.setGeometry((size.width()) / 2 - 150, 990, 300, 50)
+        self.connect(sov_hov_button, SIGNAL('clicked()'), self.sov_hov)
 
         activity_travel_pattern_button = QPushButton('Activity-travel patterns for \nall individuals within the time-slice', self)
         activity_travel_pattern_button.setGeometry((size.width()) / 2 - 150, 1070, 300, 50)
-
-
+        self.connect(activity_travel_pattern_button, SIGNAL('clicked()'), self.activity_travel_pattern)
+        
+        self.configob = co
+        
+        
+    def time_slice(self):
+        diagtitle = COMPMODEL_SMSLICE
+        modelkey = MODELKEY_SMSLICE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def activity_pursued_1(self):
+        diagtitle = COMPMODEL_SMACTIVEPURSUE
+        modelkey = MODELKEY_SMACTIVEPURSUE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def assign_to_non_hhold(self):
+        diagtitle = COMPMODEL_SMACTIVEASSIGNED
+        modelkey = MODELKEY_SMACTIVEASSIGNED
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def assign_to_hhold(self):
+        diagtitle = COMPMODEL_SMASSIGNACTIVE
+        modelkey = MODELKEY_SMASSIGNACTIVE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def mode_choice_model(self):
+        diagtitle = COMPMODEL_AFTSCHACTIVITYMODE
+        modelkey = MODELKEY_AFTSCHACTIVITYMODE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def adult_individuals(self):
+        diagtitle = COMPMODEL_SMINDIVIDUAL
+        modelkey = MODELKEY_SMINDIVIDUAL
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def travel_time(self):
+        diagtitle = COMPMODEL_SMTRIPTIME
+        modelkey = MODELKEY_SMTRIPTIME
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def activity_choice(self):
+        diagtitle = COMPMODEL_ACTIVITYTYPE
+        modelkey = MODELKEY_ACTIVITYTYPE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def actual_start_time(self):
+        diagtitle = COMPMODEL_SMSTARTTIME
+        modelkey = MODELKEY_SMSTARTTIME
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def time_in_activity(self):
+        diagtitle = COMPMODEL_ACTIVITYDURATION
+        modelkey = MODELKEY_ACTIVITYDURATION
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def proceed_next_activity(self):
+        diagtitle = COMPMODEL_SMPROCEED
+        modelkey = MODELKEY_SMPROCEED
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def mode_choice_next_activity(self):
+        diagtitle = COMPMODEL_FIXEDACTIVITYMODE
+        modelkey = MODELKEY_FIXEDACTIVITYMODE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def hov(self):
+        diagtitle = COMPMODEL_SMISHOV
+        modelkey = MODELKEY_SMISHOV
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def activity_pursued_2(self):
+        diagtitle = COMPMODEL_SMACTIVEPURSED
+        modelkey = MODELKEY_SMACTIVEPURSED
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def check_if_join_activity(self):
+        diagtitle = COMPMODEL_JOINTACTIVITY
+        modelkey = MODELKEY_JOINTACTIVITY
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def activity_non_hhold(self):
+        diagtitle = COMPMODEL_SMACTIVENON
+        modelkey = MODELKEY_SMACTIVENON
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def activity_hhold(self):
+        diagtitle = COMPMODEL_SMACTIVEHOUSE
+        modelkey = MODELKEY_SMACTIVEHOUSE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def sov_hov(self):
+        diagtitle = COMPMODEL_TRIPVEHICLE
+        modelkey = MODELKEY_TRIPVEHICLE
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+    def activity_travel_pattern(self):
+        diagtitle = COMPMODEL_SMPATTERN
+        modelkey = MODELKEY_SMPATTERN
+        
+        diag = AbtractSpecDialog(self.configob,modelkey,diagtitle)
+        diag.exec_()
+        
+        
 
     def paintEvent(self, parent = None):
         # Drawing line
