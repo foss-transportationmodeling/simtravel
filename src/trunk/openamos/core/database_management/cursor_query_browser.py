adc474d3f0604d44a7f0f97229f9c49a7ce16def
bapat.namrata <bapat.namrata@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-09-30 16:31:57 +0000

    query browser class using cursor

---
+++ src/trunk/openamos/core/database_management/cursor_query_browser.py
@@ -0,0 +1,241 @@
+#main class. this class will be used to define the database connection.
+#it will create/drop database, schema and tables
+ 
+
+#include all the import 
+import sys
+import os
+import exceptions
+import time
+import sqlalchemy
+import psycopg2 as dbapi2
+from database_connection import DataBaseConnection
+from psycopg2 import extensions
+from sqlalchemy.types import Integer, SmallInteger, \
+			     Numeric, Float, \
+			     VARCHAR, String, CLOB, Text,\
+			     Boolean, DateTime
+
+
+class MainClass(object):
+    #initialize the class 
+    def __init__(self, protocol = None, user_name = None,
+                    password = None, host_name = None, 
+                    database_name = None, engine = None, 
+                    connection = None, result = None):
+
+        self.protocol = protocol
+        self.user_name = user_name
+        self.password = password
+        self.host_name = host_name
+        self.database_name = database_name
+        self.connection = None
+        self.result = None
+        self.cursor = None
+        dbcon_obj = DataBaseConnection(self.protocol, self.user_name, self.password, self.host_name, self.database_name)
+        self.dbcon_obj = dbcon_obj
+        print self.dbcon_obj
+    
+    
+    ########## methods for select query  ##########
+    #select all rows from the table
+    def select_all_from_table(self, table_name):
+        """
+        This method is used to fetch all rows from the table specified.
+
+        Input:
+        Class name corresponding to the table
+
+        Output:
+        Returns all the rows in the table
+        """
+        try:    
+            self.dbcon_obj.cursor.execute("SELECT * FROM %s"%table_name)
+            tables = self.dbcon_obj.cursor.fetchall()
+            tbs = [tb for tb in tables]
+            return self.dbcon_obj.cursor, tbs
+        except Exception, e:
+            print 'Error while retreiving the data from the table'
+            print e
+    
+  
+    #select rows based on a selection criteria
+    def fetch_selected_rows(self, table_name, column_name, value):
+        """
+        This method is used to fetch selected rows fom the table in the database.
+
+        Input:
+        Database configuration object, class name and selection criteria.
+
+        Output:
+        Returns the rows that satisfy the selection criteria
+        """
+        print 'table name is ', table_name
+        print 'column name is ', column_name
+        print 'value is ', value
+        try:
+            self.dbcon_obj.cursor.execute("SELECT * FROM %s where %s = '%s'"%(table_name, column_name, value))
+            data = self.dbcon_obj.cursor.fetchall()
+
+            row_list = []
+            counter = 0
+            for each in data:
+                counter = counter + 1
+                row_list.append(each)
+
+            if counter == 0:
+                print 'No rows selected.\n'           
+            print 'Select query successful.\n'
+            return self.dbcon_obj.cursor, row_list
+        except Exception, e:
+            print 'Error retrieving the information. Query failed.\n'
+            print e
+    #select join query pending
+    ########## methods for select query end ##########
+    
+    
+    ########## methods for delete query  ##########    
+    #delete rows based on a deletion criteria
+    def delete_selected_rows(self, table_name, column_name, value):
+        """
+        This method is used to delete selected rows fom the table in the database.
+
+        Input:
+        Database configuration object, table name and selection criteria
+
+        Output:
+        Deletes the rows that satisfy the selection criteria
+        """
+        try:
+            self.dbcon_obj.cursor.execute("delete FROM %s where %s = '%s'"%(table_name, column_name, value))
+            self.dbcon_obj.connection.commit()
+            print 'Delete successful'
+        except Exception, e:
+            print e
+            print 'Error deleting the information. Query failed.'
+
+
+    #delete all rows i.e. empty table
+    def delete_all(self, table_name):
+        """
+        This method is used to delete all rows from the table.
+
+        Input:
+        Database configuration object, table name
+
+        Output:
+        Deletes all rows in the table
+        """
+        try:
+            self.dbcon_obj.cursor.execute("delete FROM %s "%table_name)
+            self.dbcon_obj.connection.commit()
+            print 'Delete all records successful.'
+        except Exception, e:
+            print e
+            print 'Error retrieving the information. Query failed.'
+    ########## methods for delete query end ##########
+
+
+    ########## methods for insert query     ##########
+     #insert values in the table
+    def insert_into_table(self, data_arr, table_name):
+        try:
+            print 'time before insert query ', time.time()
+            arr_str = [tuple(each) for each in data_arr]
+            arr_str = str(arr_str)[1:-1]
+            insert_stmt = "insert into %s values %s"%(table_name, arr_str)
+            #insert_stmt = "copy school from '/home/namrata/Documents/DBclasses/myfile.csv' with delimiter as ',' csv header"
+            result = self.dbcon_obj.cursor.execute(insert_stmt)
+            self.dbcon_obj.connection.commit()
+            print 'time after insert query ', time.time()
+        except Exception, e:
+            print e
+    
+    ########## methods for delete query end ##########
+
+
+    ########## methods for creating and deleting index##########
+    #create an index
+    def create_index(self, table_name, col_list):
+        index_stmt = ''
+        columns = ''
+        count = 0
+        index_name = table_name + '_index'
+
+        for i in col_list:
+            if count < (len(col_list)-1):
+                columns = columns + i + ', '
+                count = count + 1
+            else:
+                columns = columns + i
+        index_stmt = 'create index %s on %s (%s)'%(index_name, table_name, columns)
+        print index_stmt
+        try:
+            self.dbcon_obj.cursor.execute(index_stmt)
+            self.dbcon_obj.connection.commit()
+            print 'Index %s created'%index_name
+        except Exception, e:
+            print 'Error while creating an index'
+            print e
+
+
+    #delete an index
+    def delete_index(self, table_name):
+        index_name = table_name + '_index'
+        try:
+            self.dbcon_obj.cursor.execute("drop index %s"%index_name)
+            self.dbcon_obj.connection.commit()
+            print 'Index %s deleted'%index_name
+        except Exception, e:
+            print 'Error while creating an index'
+            print e
+        
+    ########## methods for creating and deleting index##########
+
+
+
+#unit test to test the code
+import unittest
+
+#define a class for testing
+class TestMainClass(unittest.TestCase):
+    #only initialize objects here
+    def setUp(self):
+        self.protocol = 'postgres'		
+        self.user_name = 'postgres'
+        self.password = '1234'
+        self.host_name = 'localhost'
+        self.database_name = 'postgres'
+
+    
+    def testMainClass(self):
+        newobject = MainClass(self.protocol, self.user_name, self.password, self.host_name, self.database_name)
+
+        """ create a connection to the database """
+        newobject.dbcon_obj.new_connection()
+        
+        table_name = 'asu'
+        column_name = 'role_id'
+        value = '1'
+        #newobject.select_all_from_table(table_name)
+
+        #newobject.fetch_selected_rows(table_name, column_name, value)
+        
+        #newobject.delete_selected_rows(table_name, column_name, value)
+
+        #newobject.delete_all(table_name)
+        
+        col_list = ['grad', 'role_id']
+        #newobject.create_index(table_name, col_list)
+        
+        #newobject.delete_index(table_name)
+
+        #data_arr = [('aa','aa','1'),('bb','bb','1')]
+        #newobject.insert_into_table(data_arr, table_name)
+        
+        """ close the connection to the database """
+        newobject.dbcon_obj.close_connection()
+
+
+if __name__ == '__main__':
+    unittest.main()
