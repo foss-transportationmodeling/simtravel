62072e93bc379ed74e91ac37afdd49a9d7bd8ae0
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-10-16 01:03:10 +0000

    table definitions modifed

--- src/trunk/openamos/core/cache/dataset_table_layouts.py
+++ src/trunk/openamos/core/cache/dataset_table_layouts.py
@@ -1,36 +1,9 @@
 import tables as t
 import time
 
-class Households(t.IsDescription):
-    houseid = t.Int32Col()
-    drvrcnt = t.Int32Col()
-    inclt35k = t.Int32Col()
-    numchild = t.Int32Col()
-    ownhome = t.Int32Col()
-    urb = t.Int32Col()
-    one = t.Int32Col()
-    zero = t.Int32Col()
-    htaz = t.Int32Col()
-
-class Persons(t.IsDescription):
-    houseid = t.Int32Col()
-    personid = t.Int32Col()
-    wtaz = t.Int32Col()
-    r_age = t.Int32Col()
-    r_sex = t.Int32Col()
-    one = t.Int32Col()
-    zero = t.Int32Col()
-    coled = t.Int32Col()
-    male = t.Int32Col()
-    drvr = t.Int32Col()
-    aglte18 = t.Int32Col()
-    aggte65 = t.Int32Col()
-    timetowrk = t.Int32Col()
-    wrkr = t.Int32Col()
-
 class Travel_Skims(t.IsDescription):
-    origin = t.Int16Col()
-    destination = t.Int16Col()
+    origin = t.Int32Col()
+    destination = t.Int32Col()
     tt = t.Float32Col()
 
 
@@ -42,26 +15,26 @@ class Persons_R(t.IsDescription):
 
 class Vehicles_R(t.IsDescription):
     houseid = t.Int32Col()
-    vehid = t.Int16Col()
-    vehtype = t.Int16Col()
+    vehid = t.Int32Col()
+    vehtype = t.Int32Col()
 
 
 class Households_R(t.IsDescription):
     houseid = t.Int32Col()
-    numvehs = t.Int16Col()
+    numvehs = t.Int32Col()
 
 class Tsp_R(t.IsDescription):
     houseid = t.Int32Col()
-    personid = t.Int16Col()
-    daystart = t.Float32Col()
-    dayend = t.Float32Col()
+    personid = t.Int32Col()
+    daystart = t.Int32Col()
+    dayend = t.Int32Col()
 	
 class Schedule_R(t.IsDescription):
     houseid = t.Int32Col()
-    personid = t.Int16Col()
-    scheduleid = t.Int16Col()
-    activitytype = t.Int16Col()
-    locationid = t.Int16Col()
-    starttime = t.Float32Col()
-    endtime = t.Float32Col()
-    duration = t.Float32Col()
+    personid = t.Int32Col()
+    scheduleid = t.Int32Col()
+    activitytype = t.Int32Col()
+    locationid = t.Int32Col()
+    starttime = t.Int32Col()
+    endtime = t.Int32Col()
+    duration = t.Int32Col()
