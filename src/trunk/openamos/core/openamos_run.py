12479af8d6023acff39707029e3f77cd7ebc4897
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-11-13 03:25:59 +0000

    the name of the function in the openamos run was modified; the implementation however should be unaffected

--- src/trunk/openamos/core/openamos_run.py
+++ src/trunk/openamos/core/openamos_run.py
@@ -3,7 +3,7 @@ import sys
 from openamos.core.run.simulation_manager_cursor import SimulationManager
 from openamos.core.errors import ArgumentsError
 
-def main():
+def run_long_term_choices():
     """
     Runs the OpenAMOS program to simulate the activity-travel choices as 
     indicated by the models and their specifications in the config file.
@@ -25,11 +25,12 @@ def main():
     simulationManagerObject = SimulationManager(fileLoc = args[0])
     simulationManagerObject.setup_databaseConnection()
     simulationManagerObject.setup_cacheDatabase('w')
+    simulationManagerObject.setup_location_information()
     simulationManagerObject.setup_tod_skims()
     simulationManagerObject.parse_config()
     simulationManagerObject.clean_database_tables()
     simulationManagerObject.run_components()
     simulationManagerObject.close_connections()
-    
+
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(run_long_term_choices())
