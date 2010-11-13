b7d947a17357fad2d32012836e9c57829b12eab6
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-11-13 18:26:53 +0000

    new set of components for the location choices

---
+++ src/trunk/openamos/core/activity_travel/activity_travel_components.py
@@ -0,0 +1,10 @@
+
+class HistoryInfo(object):
+    def __init__(self, tableName, historyAggVar, historyVarAggConditions):
+        self.tableName = tableName
+        self.historyAggVar = historyAggVar
+        self.historyVarAggConditions = historyVarAggConditions
+
+
+
+    
