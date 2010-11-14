d08e824eae898278b1dc470c2d6ef5257981a933
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-11-14 16:19:07 +0000

    additional component definitions added to deal with child dependency and allocation

--- src/trunk/openamos/core/activity_travel/activity_travel_components.py
+++ src/trunk/openamos/core/activity_travel/activity_travel_components.py
@@ -7,4 +7,19 @@ class HistoryInfo(object):
 
 
 
+
+class HouseholdStructureInfo(object):
+    def __init__(self, tableName, houseid,
+                 personid, structuresDict):
+        self.tableName = tableName
+        self.houseid = houseid
+        self.personid = personid
+        self.structuresDict = structuresDict
+
+
+    def __repr__(self):
+        return ("""Tablename - %s, keys - %s, %s"""\
+                    """\n\tStructures - %s """
+                %(self.tableName, self.houseid, self.personid, self.structuresDict))
+
     
