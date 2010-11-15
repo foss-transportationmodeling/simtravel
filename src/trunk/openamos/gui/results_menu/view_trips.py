4052a0c83e7025b1746b6455a0f192346fd10c78
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-11-15 18:37:47 +0000

    Fix GUI

--- src/trunk/openamos/gui/results_menu/view_trips.py
+++ src/trunk/openamos/gui/results_menu/view_trips.py
@@ -18,10 +18,16 @@ class MakePlot(Matplot):
         self.connects(config)
         self.cursor = self.new_obj.cursor
         self.table = table
+        #self.makeTempTables(self.cursor,table)
+
+            
         if self.isValid():
             self.valid = True
-            self.setWindowTitle("Average Absolute Relative Difference Distribution")
-            self.setWindowIcon(QIcon("./images/region.png"))
+            if table.lower() == 'trips':
+                self.setWindowTitle("Trip Characteristics")
+            else:
+                self.setWindowTitle("Activity Characteristics")
+            #self.setWindowIcon(QIcon("./images/region.png"))
             aardWarning = QLabel("""<font color = blue>Note: The above chart shows the distribution of the """
                                  """Average Absolute Relative Difference (AARD)"""
                                  """ across all geographies for which a synthetic population was generated. """
@@ -33,7 +39,7 @@ class MakePlot(Matplot):
             aardWarning.setWordWrap(True)
             self.fill_variable1(self.table)
             self.vbox.addWidget(self.addfilter)
-            self.vbox.addWidget(aardWarning)
+            #self.vbox.addWidget(aardWarning)
             self.vbox.addWidget(self.canvas)
             self.vbox.addWidget(self.dialogButtonBox)
             self.setLayout(self.vbox)
@@ -41,11 +47,21 @@ class MakePlot(Matplot):
             if self.table == "trips":
                 self.xtitle = self.trip_labels(0)
                 self.labelsdict = {}
-                self.labelsdict['purpose'] = self.trip_labels(1)
-                self.labelsdict['mode'] = self.trip_labels(2)
-                self.labelsdict['strttime'] = self.trip_labels(3)
-                self.labelsdict['endtime'] = self.trip_labels(4)
-                self.labelsdict['occupancy'] = self.trip_labels(5)
+                self.labelsdict['purpose_rec'] = self.trip_labels(1)
+                self.labelsdict['mode_rec'] = self.trip_labels(2)
+                self.labelsdict['strttime_rec'] = self.trip_labels(3)
+                self.labelsdict['endtime_rec'] = self.trip_labels(4)
+                self.labelsdict['occupancy_rec'] = self.trip_labels(5)
+                self.labelsdict['duration_rec'] = self.trip_labels(6)
+                self.labelsdict['miles_rec'] = self.trip_labels(7)
+            else:
+                self.xtitle = self.schedule_labels(0)
+                self.labelsdict = {}
+                self.labelsdict['activitytype'] = self.schedule_labels(1)
+                self.labelsdict['strttime_rec'] = self.schedule_labels(2)
+                self.labelsdict['endtime_rec'] = self.schedule_labels(3)
+                self.labelsdict['duration_rec'] = self.schedule_labels(4)
+                #self.labelsdict['locationid'] = self.schedule_labes(5)
             
             
             self.connect(self.choicevar1, SIGNAL("currentIndexChanged(int)"), self.on_draw1)
@@ -56,10 +72,80 @@ class MakePlot(Matplot):
     def isValid(self):
         return self.checkIfTableExists(self.table)
 
+
+    def updatecolumns(self):
+        try:
+            table_name = ""
+            if self.table == "trips":
+                table_name = "temptrips"
+            else:
+                table_name = 'tempschedule'
+                
+            self.cursor.execute("UPDATE %s SET strttime_rec = 1 WHERE strttime >= 0 AND strttime < 120"%(table_name))
+            self.cursor.execute("UPDATE %s SET strttime_rec = 2 WHERE strttime >= 120 AND strttime < 300"%(table_name))
+            self.cursor.execute("UPDATE %s SET strttime_rec = 3 WHERE strttime >= 300 AND strttime < 480"%(table_name))
+            self.cursor.execute("UPDATE %s SET strttime_rec = 4 WHERE strttime >= 480 AND strttime < 660"%(table_name))
+            self.cursor.execute("UPDATE %s SET strttime_rec = 5 WHERE strttime >= 660 AND strttime < 900"%(table_name))
+            self.cursor.execute("UPDATE %s SET strttime_rec = 6 WHERE strttime >= 900"%(table_name))
+            
+            self.cursor.execute("UPDATE %s SET endtime_rec = 1 WHERE endtime >= 0 AND endtime < 120"%(table_name))
+            self.cursor.execute("UPDATE %s SET endtime_rec = 2 WHERE endtime >= 120 AND endtime < 300"%(table_name))
+            self.cursor.execute("UPDATE %s SET endtime_rec = 3 WHERE endtime >= 300 AND endtime < 480"%(table_name))
+            self.cursor.execute("UPDATE %s SET endtime_rec = 4 WHERE endtime >= 480 AND endtime < 660"%(table_name))
+            self.cursor.execute("UPDATE %s SET endtime_rec = 5 WHERE endtime >= 660 AND endtime < 900"%(table_name))
+            self.cursor.execute("UPDATE %s SET endtime_rec = 6 WHERE endtime >= 900"%(table_name))
+            
+            if self.table == "trips":
+                
+                self.cursor.execute("UPDATE tempschedule SET duration_rec = 1 WHERE duration >= 0 AND duration <= 10")
+                self.cursor.execute("UPDATE tempschedule SET duration_rec = 2 WHERE duration >= 11 AND duration <= 30")
+                self.cursor.execute("UPDATE tempschedule SET duration_rec = 3 WHERE duration >= 31 AND duration <= 120")
+                self.cursor.execute("UPDATE tempschedule SET duration_rec = 4 WHERE duration >= 121 AND duration <= 240")
+                self.cursor.execute("UPDATE tempschedule SET duration_rec = 5 WHERE duration > 240")
+        
+        except Exception, e:
+            print '\tError while creating the table %s'%self.table_name
+            print e
+            
+
+    def makecolumns(self):
+        try:
+            if self.table == "trips":
+                if not self.checkColumnExists("temptrips","strttime_rec"):
+                    self.cursor.execute("ALTER TABLE temptrips ADD COLUMN strttime_rec bigint")
+                if not self.checkColumnExists("temptrips","endtime_rec"):
+                    self.cursor.execute("ALTER TABLE temptrips ADD COLUMN endtime_rec bigint")
+            else:
+                if not self.checkColumnExists("tempschedule","strttime_rec"):
+                    self.cursor.execute("ALTER TABLE tempschedule ADD COLUMN strttime_rec bigint")
+                if not self.checkColumnExists("tempschedule","endtime_rec"):
+                    self.cursor.execute("ALTER TABLE tempschedule ADD COLUMN endtime_rec bigint")
+                if not self.checkColumnExists("tempschedule","duration_rec"):
+                    self.cursor.execute("ALTER TABLE tempschedule ADD COLUMN duration_rec bigint")
+        
+        except Exception, e:
+            print '\tError while creating the table %s'%self.table_name
+            print e
+        
+            
+    def makeTempTables(self,cursor,table):
+        try:
+            if table == "trips":
+                if not self.checkIfTableExists("temptrips"):
+                    cursor.execute("CREATE TABLE temptrips AS SELECT * FROM %s"%(table))
+            else:
+                if not self.checkIfTableExists("tempschedule"):
+                    cursor.execute("CREATE TABLE tempschedule AS SELECT * FROM %s"%(table))
+        
+        except Exception, e:
+            print '\tError while creating the table %s'%self.table_name
+            print e
+
+        
     def on_draw1(self):
         """ Redraws the figure
         """
-        if not self.selectvar2.isChecked() and str(self.choicevar1.currentIndex())>0:
+        if not self.selectvar2.isChecked() and int(self.choicevar1.currentIndex())>0:
             self.err1 = []
             self.err2 = []          
             if self.retrieveResults():
@@ -71,14 +157,16 @@ class MakePlot(Matplot):
         
                 #self.axes.hist(self.err, range=(1,10), normed=True, cumulative=False, histtype='bar', align='mid', orientation='vertical', log=False)
                 #self.axes.hist(self.err, normed=False, align='left')
-                self.axes.bar(ind, self.err2, align='center')
+                self.axes.bar(ind, self.err2, color='green', align='center')
                 self.axes.set_xlabel(self.xtitle[str(self.choicevar1.currentText())])
                 self.axes.set_ylabel("Percent (%)")
                 
                 labelsdict = self.labelsdict[str(self.choicevar1.currentText())]
                 labels = []
                 for label in self.err1:
-                    temp = labelsdict[label]
+                    temp = label
+                    if label in labelsdict.keys():
+                        temp = labelsdict[label]
                     labels.append(temp)
                     
                 self.axes.set_xticks(ind)
@@ -90,6 +178,10 @@ class MakePlot(Matplot):
                     self.axes.set_xticklabels(labels)
             
                 self.canvas.draw()
+                
+        elif self.selectvar2.isChecked() and int(self.choicevar1.currentIndex())>0 and int(self.choicevar2.currentIndex()) > 0:
+            self.on_draw2()
+            
 
                 
 
@@ -98,7 +190,11 @@ class MakePlot(Matplot):
 
         #self.cursor = self.new_obj.cursor
         vars = self.choicevar1.currentText() + ", count(*)"
-        tablename = self.table
+        tablename = ""
+        if self.table == "trips":
+            tablename = "temptrips"
+        else:
+            tablename = "tempschedule"
         filter = self.choicevar1.currentText() + " >= 0"
         group = self.choicevar1.currentText()
         order = self.choicevar1.currentText()
@@ -147,7 +243,11 @@ class MakePlot(Matplot):
             for var in bound:
                 
                 vars = "%s, count(*)" %(self.choicevar2.currentText())
-                tablename = self.table
+                tablename = ""
+                if self.table == "trips":
+                    tablename = "temptrips"
+                else:
+                    tablename = "tempschedule"
                 filter = "%s = %s and %s >= 0 and %s >= 0" %(self.choicevar1.currentText(),var,
                                                              self.choicevar1.currentText(),self.choicevar2.currentText())
                 group = self.choicevar2.currentText()
@@ -185,8 +285,11 @@ class MakePlot(Matplot):
             labelsdict = self.labelsdict[str(self.choicevar2.currentText())]
             labels = []
             for label in self.err1:
-                temp = labelsdict[label]
+                temp = label
+                if label in labelsdict.keys():
+                    temp = labelsdict[label]
                 labels.append(temp)
+
                 
             self.axes.set_xticks(ind)
             if len(labels) >= 13:
@@ -199,8 +302,11 @@ class MakePlot(Matplot):
             legenddict = self.labelsdict[str(self.choicevar1.currentText())]
             legendlabel = []
             for label in bound:
-                temp = legenddict[label]
+                temp = label
+                if label in legenddict.keys():
+                    temp = legenddict[label]
                 legendlabel.append(temp)
+
                 
             self.axes.legend(bars,legendlabel,prop=prop,bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
             self.axes.set_xlabel(self.xtitle[str(self.choicevar2.currentText())])
@@ -213,7 +319,11 @@ class MakePlot(Matplot):
     def boundvalue(self):
 
         vars = self.choicevar1.currentText()
-        tablename = self.table
+        tablename = ""
+        if self.table == "trips":
+            tablename = "temptrips"
+        else:
+            tablename = "tempschedule"
         filter = "%s >= 0" %(self.choicevar1.currentText())
         group = self.choicevar1.currentText()
         order = self.choicevar1.currentText()
@@ -236,7 +346,11 @@ class MakePlot(Matplot):
 
         if str(self.choicevar1.currentIndex())>0 and str(self.choicevar2.currentIndex())>0:
             vars = "%s, count(*)" %(self.choicevar2.currentText())
-            tablename = self.table
+            tablename = ""
+            if self.table == "trips":
+                tablename = "temptrips"
+            else:
+                tablename = "tempschedule"
             filter = "%s >= 0 and %s >= 0" %(self.choicevar1.currentText(),self.choicevar2.currentText())
             group = self.choicevar2.currentText()
             order = self.choicevar2.currentText()
@@ -268,24 +382,17 @@ class MakePlot(Matplot):
     
     def trip_labels(self, index):
     
-        xtitle = {'purpose':'Trip Purpose','strttime':'Trip Starting Time','endtime':'Trip Ending Time',
-                       'mode':'Transportation Mode','occupancy':'Occupancies'}         
-        purposedict = {0:'Home',1:'Work',2:'School',3:'Pers Buss',4:'Shopping',5:'Social',6:'Recreation',7:'Meals',
-                       8:'Serve Pass',9:'Other'}
-        modedict = {1:'Car',2:'Van',3:'SUV',4:'Pickup',5:'Truck',6:'RV',7:'Motorcycle',8:'Gold Cart',9:'Public Bus',
-                    10:'Commu Bus',11:'School Bus',12:'Charter',13:'City-City Bus',14:'Shuttle',15:'Amtrak',
-                    16:'Commu Train',17:'Subway',18:'Trolley',19:'Taxicab',20:'Ferry',21:'Airplane',22:'Bicycle',
-                    23:'Walk',24:'Wheelchair',97:'Other'}
-        strttime = {1:'0 to 1',2:'1 to 2',3:'2 to 3',4:'3 to 4',5:'4 to 5',6:'5 to 6',7:'6 to 7',8:'7 to 8',
-                    9:'8 to 9',10:'9 to 10',11:'10 to 11',12:'11 to 12',13:'12 to 13',14:'13 to 14',15:'14 to 15',
-                    16:'15 to 16',17:'16 to 17',18:'17 to 18',19:'18 to 19',20:'19 to 20',21:'20 to 21',
-                    22:'21 to 22',23:'22 to 23',24:'23 to 24'}
-        endtime = {1:'0 to 1',2:'1 to 2',3:'2 to 3',4:'3 to 4',5:'4 to 5',6:'5 to 6',7:'6 to 7',8:'7 to 8',
-                   9:'8 to 9',10:'9 to 10',11:'10 to 11',12:'11 to 12',13:'12 to 13',14:'13 to 14',15:'14 to 15',
-                   16:'15 to 16',17:'16 to 17',18:'17 to 18',19:'18 to 19',20:'19 to 20',21:'20 to 21',
-                   22:'21 to 22',23:'22 to 23',24:'23 to 24'}
-        accmp = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',
-                 9:'9',10:'10',11:'11',12:'12',13:'13',14:'14',15:'15'}
+        xtitle = {'purpose_rec':'Trip Purpose','strttime_rec':'Trip Start Time','endtime_rec':'Trip End Time',
+                       'mode_rec':'Trip Mode','occupancy_rec':'Occupancy','duration_rec':'Trip Time (mins)','miles_rec':'Trip Length (miles)'}         
+        purposedict = {0:'Return Home',1:'Work',2:'School',3:'Pers Buss',4:'Shopping',5:'Social Visit',6:'Sports/Rec',7:'Meal',
+                       8:'Serve Passgr',9:'Other'}
+        modedict = {1:'Car',2:'Van',3:'SUV',4:'Pickup Truck',5:'Bus',6:'Train',7:'School Bus',8:'Bike',9:'Walk',
+                    10:'Taxi',11:'Other'}
+        strttime = {1:'4am-6am',2:'6am-9am',3:'9am-12pm',4:'12pm-3pm',5:'3pm-7pm',6:'after 7pm'}
+        endtime = {1:'4am-6am',2:'6am-9am',3:'9am-12pm',4:'12pm-3pm',5:'3pm-7pm',6:'after 7pm'}
+        occupancy = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5 or more'}
+        duration = {1:'0-10',2:'11-30',3:'31-120',4:'121-240',5:'> 240'}
+        miles = {1:'0-5',2:'6-15',3:'16-30',4:'31-50',5:'> 50'}
         
         if index == 0:
             return xtitle
@@ -298,8 +405,32 @@ class MakePlot(Matplot):
         if index == 4:
             return endtime
         if index == 5:
-            return accmp
+            return occupancy
+        if index == 6:
+            return duration
+        if index == 7:
+            return miles
         
+        
+    def schedule_labels(self, index):
+        xtitle = {'activitytype':'Activity Type','strttime_rec':'Start Time','endtime_rec':'End Time',
+                  'duration_rec':'Activity Duration (mins)'}
+        activitytype = {100:'In-home',200:'Work',300:'School',411:'Pers Buss',412:'Shopping',
+                        415:'Meal',416:'Serve Passgr',513:'Social Visit',514:'Sports/Rec',900:'Other'}
+        strttime = {1:'4am-6am',2:'6am-9am',3:'9am-12pm',4:'12pm-3pm',5:'3pm-7pm',6:'after 7pm'}
+        endtime = {1:'4am-6am',2:'6am-9am',3:'9am-12pm',4:'12pm-3pm',5:'3pm-7pm',6:'after 7pm'}
+        duration = {1:'0-10',2:'11-30',3:'31-120',4:'121-240',5:'> 240'}
+
+        if index == 0:
+            return xtitle
+        if index == 1:
+            return activitytype
+        if index == 2:
+            return strttime
+        if index == 3:
+            return endtime
+        if index == 4:
+            return duration
         
     def dropVar2(self):
         if not self.selectvar2.isChecked():
