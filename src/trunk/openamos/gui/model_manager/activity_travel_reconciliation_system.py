4c4e3700620b523567167ff25fdd0131b4805439
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-11-15 00:41:16 +0000

    Clear the flowchart

--- src/trunk/openamos/gui/model_manager/activity_travel_reconciliation_system.py
+++ src/trunk/openamos/gui/model_manager/activity_travel_reconciliation_system.py
@@ -16,34 +16,21 @@ class Travel_Reconciliation_System(QWidget):
         global widgetwidth, widgetheight
         widgetwidth = size.width()
         widgetheight = size.height()
-        
-        skeleton_onciliation_button = QPushButton('Activity \nSkeleton Reconciliation', self)
-        skeleton_onciliation_button.setGeometry((size.width()) / 2 - 300, size.height() / 2 - 220, 200, 80)
-        #self.connect(skeleton_onciliation_button, SIGNAL('clicked()'), self.skeleton_reconciliation)
-        #self.connect(workersbutton, SIGNAL('clicked()'), qApp, SLOT('Close()'))
-        
-        person_constraints_1_button = QPushButton("Within person constraints", self)     
-        person_constraints_1_button.setGeometry((size.width()) / 2 - 300, size.height() / 2 - 110, 200, 80)
-        #self.connect(person_constraints_1_button, SIGNAL('clicked()'), self.person_constraints_1)
-        
-        adjustment_1_button = QPushButton('Adjustments to the activity \nskeleton based on expected \nTravel Time from previous day', self)
-        adjustment_1_button.setGeometry((size.width()) / 2 - 300, size.height() / 2, 200, 80)
-        #self.connect(adjustment_1_button, SIGNAL('clicked()'), self.adjustment_1)
 
         pattern_onciliation_button = QPushButton('Activity-travel \nPattern Reconciliation', self)
-        pattern_onciliation_button.setGeometry((size.width()) / 2 + 100, size.height() / 2 - 220, 200, 80)
+        pattern_onciliation_button.setGeometry((size.width()) / 2 - 100, size.height() / 2 - 270, 200, 80)
         #self.connect(pattern_onciliation_button, SIGNAL('clicked()'), self.pattern_reconciliation)
 
         person_constraints_2_button = QPushButton("Within person constraints", self)     
-        person_constraints_2_button.setGeometry((size.width()) / 2 + 100, size.height() / 2 - 110, 200, 80)
+        person_constraints_2_button.setGeometry((size.width()) / 2 - 100, size.height() / 2 - 160, 200, 80)
         #self.connect(person_constraints_2_button, SIGNAL('clicked()'), self.person_constraints_2)
 
         hhold_constraints_button = QPushButton("Within household constraints", self)     
-        hhold_constraints_button.setGeometry((size.width()) / 2 + 100, size.height() / 2, 200, 80)
+        hhold_constraints_button.setGeometry((size.width()) / 2 - 100, size.height() / 2 - 50, 200, 80)
         #self.connect(hhold_constraints_button, SIGNAL('clicked()'), self.hhold_constraints)
 
         adjustment_2_button = QPushButton('Duration adjustment \nafter arrival', self)
-        adjustment_2_button.setGeometry((size.width()) / 2 + 100, size.height() / 2 + 110, 200, 80)
+        adjustment_2_button.setGeometry((size.width()) / 2 - 100, size.height() / 2 + 60, 200, 80)
         #self.connect(adjustment_2_button, SIGNAL('clicked()'), self.adjustment_2)
         
         Dummy  = QPushButton('', self)
@@ -108,36 +95,36 @@ class Travel_Reconciliation_System(QWidget):
         line.begin(self)
         pen = QPen(Qt.black,2,Qt.SolidLine)
         line.setPen(pen)
-        line.drawLine(widgetwidth / 2 - 200, widgetheight / 2 - 140, widgetwidth / 2 - 200, widgetheight / 2)
-        line.drawLine(widgetwidth / 2 + 200, widgetheight / 2 - 140, widgetwidth / 2 + 200, widgetheight / 2 + 110)
+        #line.drawLine(widgetwidth / 2 - 200, widgetheight / 2 - 140, widgetwidth / 2 - 200, widgetheight / 2)
+        line.drawLine(widgetwidth / 2, widgetheight / 2 - 190, widgetwidth / 2, widgetheight / 2 + 60)
         line.end()
         # Drawing arrow
         arrow = QPainter()
         arrow.begin(self)
         point = QPoint()
-        point.setX(widgetwidth / 2 - 200)  
-        point.setY(widgetheight / 2 - 110)
+#        point.setX(widgetwidth / 2 - 200)  
+#        point.setY(widgetheight / 2 - 110)
+#        arrow.setBrush(QColor("black"))
+#        arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
+#
+#        point.setX(widgetwidth / 2 - 200)  
+#        point.setY(widgetheight / 2)
+#        arrow.setBrush(QColor("black"))
+#        arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
+
+        point.setX(widgetwidth / 2)  
+        point.setY(widgetheight / 2 - 160)
         arrow.setBrush(QColor("black"))
         arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
 
-        point.setX(widgetwidth / 2 - 200)  
-        point.setY(widgetheight / 2)
-        arrow.setBrush(QColor("black"))
-        arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
-
-        point.setX(widgetwidth / 2 + 200)  
-        point.setY(widgetheight / 2 - 110)
-        arrow.setBrush(QColor("black"))
-        arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
-
-        point.setX(widgetwidth / 2 + 200)  
-        point.setY(widgetheight / 2)
+        point.setX(widgetwidth / 2)  
+        point.setY(widgetheight / 2 - 50)
         arrow.setBrush(QColor("black"))
         arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
 
 
-        point.setX(widgetwidth / 2 + 200)  
-        point.setY(widgetheight / 2 + 110)
+        point.setX(widgetwidth / 2)  
+        point.setY(widgetheight / 2 + 60)
         arrow.setBrush(QColor("black"))
         arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
 
