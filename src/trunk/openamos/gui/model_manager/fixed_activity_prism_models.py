dbc55b1ee5be463204112c918adc59f84c999804
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-12-02 06:47:15 +0000

    Change button color

--- src/trunk/openamos/gui/model_manager/fixed_activity_prism_models.py
+++ src/trunk/openamos/gui/model_manager/fixed_activity_prism_models.py
@@ -15,117 +15,109 @@ class FixedActivityPrismModels(QWidget):
         widgetwidth = size.width()
         widgetheight = size.height()
         
-        worker_button = QPushButton('Adult Workers', self)
-        worker_button.setGeometry((size.width()) / 2 - 405, size.height() / 2 - 450, 180, 50)
-        
+        # Adult worker
         day_start1_button = QPushButton(COMPMODEL_DAYSTART, self)
         day_start1_button.setGeometry((size.width()) / 2 - 405, size.height() / 2 - 370,180, 50)
-        #day_start_button.setStyleSheet("background-color: rgb(0, 255, 0)")
-        #self.connect(workersbutton, SIGNAL('clicked()'), qApp, SLOT('Close()'))
+        day_start1_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_start1_button, SIGNAL('clicked()'), self.day_start_aw)
 
         day_end1_button = QPushButton(COMPMODEL_DAYEND, self)     
         day_end1_button.setGeometry((size.width()) / 2 - 405, size.height() / 2 - 290, 180, 50)
+        day_end1_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_end1_button, SIGNAL('clicked()'), self.day_end_aw)
         
         num_work_episodes_button = QPushButton(COMPMODEL_WRKEPISODES, self)
         num_work_episodes_button.setGeometry((size.width()) / 2 - 405, size.height() / 2 - 210, 180, 50)
+        num_work_episodes_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(num_work_episodes_button, SIGNAL('clicked()'), self.num_work)
 
         work_start_button = QPushButton(COMPMODEL_WORKSTART, self)
         work_start_button.setGeometry((size.width()) / 2 - 505, size.height() / 2 - 60, 180, 50)
+        work_start_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(work_start_button, SIGNAL('clicked()'), self.work_start)
 
         work_end_button = QPushButton(COMPMODEL_WORKEND, self)
         work_end_button.setGeometry((size.width()) / 2 - 505, size.height() / 2 + 20, 180, 50)
+        work_end_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(work_end_button, SIGNAL('clicked()'), self.work_end)
 
         work_start_1_button = QPushButton(COMPMODEL_WORKSTART1, self)
         work_start_1_button.setGeometry((size.width()) / 2 - 305, size.height() / 2 - 60, 180, 50)
+        work_start_1_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(work_start_1_button, SIGNAL('clicked()'), self.work_start1)
 
         work_end_1_button = QPushButton(COMPMODEL_WORKEND1, self)
         work_end_1_button.setGeometry((size.width()) / 2 - 305, size.height() / 2 + 20, 180, 50)
+        work_end_1_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(work_end_1_button, SIGNAL('clicked()'), self.work_end1)
         
         work_start_2_button = QPushButton(COMPMODEL_WORKSTART2, self)
         work_start_2_button.setGeometry((size.width()) / 2 - 305, size.height() / 2 + 100, 180, 50)
+        work_start_2_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(work_start_2_button, SIGNAL('clicked()'), self.work_start2)
 
         work_end_2_button = QPushButton(COMPMODEL_WORKEND2, self)
         work_end_2_button.setGeometry((size.width()) / 2 - 305, size.height() / 2 + 180, 180, 50)
+        work_end_2_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(work_end_2_button, SIGNAL('clicked()'), self.work_end2)
         
         
-        
-
-
-        non_worker_button = QPushButton('Adult Non-workers', self)
-        non_worker_button.setGeometry((size.width()) / 2 - 140, size.height() / 2 - 450, 180, 50)
-        #self.connect(non_worker_button, SIGNAL('clicked()'), self.non_worker)
-        
+        # Adult non-worker
         day_start2_button = QPushButton(COMPMODEL_DAYSTART, self)
         day_start2_button.setGeometry((size.width()) / 2 - 140, size.height() / 2 - 370,180, 50)
-        #day_start_button.setStyleSheet("background-color: rgb(0, 255, 0)")
-        #self.connect(workersbutton, SIGNAL('clicked()'), qApp, SLOT('Close()'))
+        day_start2_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_start2_button, SIGNAL('clicked()'), self.day_start_nw)
         
         day_end2_button = QPushButton(COMPMODEL_DAYEND, self)     
         day_end2_button.setGeometry((size.width()) / 2 - 140, size.height() / 2 - 290, 180, 50)
+        day_end2_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_end2_button, SIGNAL('clicked()'), self.day_end_nw)
         
         
-
-
-        children_button = QPushButton('Children (5-17 years)', self)
-        children_button.setGeometry((size.width()) / 2 + 125, size.height() / 2 - 450, 180, 50)
-        
+        # Children (5 - 17)
         day_start3_button = QPushButton(COMPMODEL_DAYSTART, self)
         day_start3_button.setGeometry((size.width()) / 2 + 125, size.height() / 2 - 370,180, 50)
-        #day_start_button.setStyleSheet("background-color: rgb(0, 255, 0)")
-        #self.connect(workersbutton, SIGNAL('clicked()'), qApp, SLOT('Close()'))
+        day_start3_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_start3_button, SIGNAL('clicked()'), self.day_start_na)
         
         day_end3_button = QPushButton(COMPMODEL_DAYEND, self)     
         day_end3_button.setGeometry((size.width()) / 2 + 125, size.height() / 2 - 290, 180, 50)
+        day_end3_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_end3_button, SIGNAL('clicked()'), self.day_end_na)
 
         children_arrive_button = QPushButton(COMPMODEL_SCHSTART, self)
         children_arrive_button.setGeometry((size.width()) / 2 + 125, size.height() / 2 - 210, 180, 50)
+        children_arrive_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(children_arrive_button, SIGNAL('clicked()'), self.school_start)
 
         children_depart_button = QPushButton(COMPMODEL_SCHEND, self)
         children_depart_button.setGeometry((size.width()) / 2 + 125, size.height() / 2 - 130, 180, 50)
+        children_depart_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(children_depart_button, SIGNAL('clicked()'), self.school_end)
 
 
-
-
-        prechildren_button = QPushButton('Preschool Children\n(0-4 years)', self)
-        prechildren_button.setGeometry((size.width()) / 2 + 390, size.height() / 2 - 450, 180, 50)
-        
+        # Children (0 - 4)
         day_start4_button = QPushButton(COMPMODEL_DAYSTART, self)
         day_start4_button.setGeometry((size.width()) / 2 + 390, size.height() / 2 - 370,180, 50)
-        #day_start_button.setStyleSheet("background-color: rgb(0, 255, 0)")
-        #self.connect(workersbutton, SIGNAL('clicked()'), qApp, SLOT('Close()'))
+        day_start4_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_start4_button, SIGNAL('clicked()'), self.day_start_ps)
         
         day_end4_button = QPushButton(COMPMODEL_DAYEND, self)     
         day_end4_button.setGeometry((size.width()) / 2 + 390, size.height() / 2 - 290, 180, 50)
+        day_end4_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(day_end4_button, SIGNAL('clicked()'), self.day_end_ps)
 
         presch_arrive_button = QPushButton(COMPMODEL_PRESCHSTART, self)
         presch_arrive_button.setGeometry((size.width()) / 2 + 390, size.height() / 2 - 210, 180, 50)
+        presch_arrive_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(presch_arrive_button, SIGNAL('clicked()'), self.preschool_start)
 
         presch_depart_button = QPushButton(COMPMODEL_PRESCHEND, self)
         presch_depart_button.setGeometry((size.width()) / 2 + 390, size.height() / 2 - 130, 180, 50)
+        presch_depart_button.setStyleSheet("background-color: #FFFDD0")
         self.connect(presch_depart_button, SIGNAL('clicked()'), self.preschool_end)
-        
 
-        t_s_prism_vertices_button = QPushButton('Time-space Prism Vertices for the Population', self)
-        t_s_prism_vertices_button.setGeometry((size.width()) / 2 - 150, size.height() / 2 + 320, 300, 50)
-        #self.connect(t_s_prism_vertices_button, SIGNAL('clicked()'), self.t_s_prism_vertices)
+        
         
         Dummy  = QPushButton('', self)
         Dummy.setGeometry(0, size.height() - 4, 1140, 2)
@@ -429,16 +421,17 @@ class FixedActivityPrismModels(QWidget):
         arrow.setBrush(QColor("black"))
         arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
 
+        arrow.setBrush(QColor("#1e90ff"))
+        #arrow.setOpacity(0.7)
+        arrow.drawRoundedRect(widgetwidth/2 - 405, widgetheight/2 - 450, 180, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/2 - 140, widgetheight/2 - 450, 180, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/2 + 125, widgetheight/2 - 450, 180, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/2 + 390, widgetheight/2 - 450, 178, 50, 15.0, 15.0)
+        arrow.setBrush(QColor("#F0F0F0"))
+        arrow.drawRoundedRect(widgetwidth/2 + 150, widgetheight/2 + 180, 350, 50, 15.0, 15.0)
         
-#        point.setX(widgetwidth / 2 + 315)  
-#        point.setY(widgetheight / 2 + 110)
-#        arrow.setBrush(QColor("black"))
-#        arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
-#        
-#        point.setX(widgetwidth / 2)  
-#        point.setY(widgetheight / 2 + 320)
-#        arrow.setBrush(QColor("black"))
-#        arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
+        arrow.end()
+
 
         text = QPainter()
         text.begin(self)
@@ -451,14 +444,23 @@ class FixedActivityPrismModels(QWidget):
         point.setX(widgetwidth / 2 - 230)  
         point.setY(widgetheight / 2 - 125)
         text.drawText(point, "Two")
+        
+        temp = QRect(widgetwidth/2 - 405, widgetheight/2 - 450, 180, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Adult Workers')
+        
+        temp = QRect(widgetwidth/2 - 140, widgetheight/2 - 450, 180, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Adult Non-workers')
 
-        arrow.end()
+        temp = QRect(widgetwidth/2 + 125, widgetheight/2 - 450, 180, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Children (5-17 years)')
+
+        temp = QRect(widgetwidth/2 + 390, widgetheight/2 - 450, 178, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Preschool Children\n(0-4 years)')
+        
+        temp = QRect(widgetwidth/2 + 150, widgetheight/2 + 180, 350, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Time-space Prism Vertices for the Population')
+
+        text.end()
+
 
         
-
-
-
-
- 
-
-
