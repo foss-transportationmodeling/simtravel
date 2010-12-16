57ed528965d8c079efddfe56363f6febee68faf7
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2010-12-16 16:50:50 +0000

    Change button color

--- src/trunk/openamos/gui/model_manager/activity_travel_pattern_simulator.py
+++ src/trunk/openamos/gui/model_manager/activity_travel_pattern_simulator.py
@@ -15,87 +15,100 @@ class Activity_Travel_Pattern_Simulator(QWidget):
         widgetwidth = size.width()
         widgetheight = size.height()
         
-        time_slice_button = QPushButton('Within a time slice', self)
-        time_slice_button.setGeometry(size.width() / 2 - 100, 40, 200, 50)
-        #self.connect(time_slice_button, SIGNAL('clicked()'), self.time_slice)
 
-        children_button = QPushButton('Children with after school \ndependent activities', self)
-        children_button.setGeometry((size.width()) * 3 / 4 - 100, 120, 200, 50)
+#        children_button = QPushButton('Children with after school \ndependent activities', self)
+#        children_button.setGeometry((size.width()) * 3 / 4 - 100, 120, 200, 50)
+#        children_button.setStyleSheet("background-color: #FFFDD0")
 
         activity_pursued_1_button = QPushButton('Can activity be pursued \njointly (Spatial and Temporal \nfeasibility) with a Household \nmember?', self)
         activity_pursued_1_button.setGeometry((size.width()) * 3 / 4 - 100, 200, 200, 70)
+        activity_pursued_1_button.setStyleSheet("background-color: #00C5CD")
         self.connect(activity_pursued_1_button, SIGNAL('clicked()'), self.activity_pursued_1)
 
         assign_to_non_hhold_button = QPushButton('Activity assigned to a \nNon-household member \ncomprising Joint Activity with \nNon-household member', self)
         assign_to_non_hhold_button.setGeometry((size.width()) * 3 / 4 + 40, 300, 220, 70)
+        assign_to_non_hhold_button.setStyleSheet("background-color: #00C5CD")
         self.connect(assign_to_non_hhold_button, SIGNAL('clicked()'), self.assign_to_non_hhold)
 
         assign_to_hhold_button = QPushButton('Assign the activity to \nHousehold member comprising\nJoint Activity with \nhousehold member', self)
         assign_to_hhold_button.setGeometry((size.width()) * 3 / 4 - 260, 300, 220, 70)
+        assign_to_hhold_button.setStyleSheet("background-color: #00C5CD")
         self.connect(assign_to_hhold_button, SIGNAL('clicked()'), self.assign_to_hhold)
 
         mode_choice_model_button = QPushButton('Mode choice model for \nintra-household joint \ntrips with children', self)
         mode_choice_model_button.setGeometry((size.width()) * 3 / 4 - 260, 400, 220, 50)
+        mode_choice_model_button.setStyleSheet("background-color: #00C5CD")
         self.connect(mode_choice_model_button, SIGNAL('clicked()'), self.mode_choice_model)
        
-        all_other_individuals_button = QPushButton('All other individuals', self)
-        all_other_individuals_button.setGeometry((size.width()) / 4 - 110, 120, 220, 50)
+#        all_other_individuals_button = QPushButton('All other individuals', self)
+#        all_other_individuals_button.setGeometry((size.width()) / 4 - 110, 120, 220, 50)
+#        all_other_individuals_button.setStyleSheet("background-color: #FFFDD0")
 
         adult_individuals_button = QPushButton('Adult individuals, children \nwith independent activities \n(if children stay at home then \nthey shadow the activity-travel \npatterns of adult to whom \nthey are assigned)', self)
         adult_individuals_button.setGeometry((size.width()) / 4 - 110, 200, 220, 100)
+        adult_individuals_button.setStyleSheet("background-color: #00C5CD")
         self.connect(adult_individuals_button, SIGNAL('clicked()'), self.adult_individuals)       
 
         travel_time_button = QPushButton('Is travel time to next \nfixed activity \74 time \navailable in the prism?', self)
         travel_time_button.setGeometry((size.width()) / 4 - 110, 330, 220, 50)
+        travel_time_button.setStyleSheet("background-color: #00C5CD")
         self.connect(travel_time_button, SIGNAL('clicked()'), self.travel_time)
 
         activity_choice_button = QPushButton('Activity Type Choice\nMode-Destination Choice; mode-\ndestination choices are limited \nby the TSP and travel time to \nnext fixed activity\nActivity Duration Choice', self)
         activity_choice_button.setGeometry((size.width()) / 4 - 260, 410, 220, 100)
+        activity_choice_button.setStyleSheet("background-color: #00C5CD")
         self.connect(activity_choice_button, SIGNAL('clicked()'), self.activity_choice)
 
         actual_start_time_button = QPushButton('Actual start time \nfor the activity', self)
         actual_start_time_button.setGeometry((size.width()) / 4 - 260, 540, 220, 50)
+        actual_start_time_button.setStyleSheet("background-color: #00C5CD")
         self.connect(actual_start_time_button, SIGNAL('clicked()'), self.actual_start_time)
 
         time_in_activity_button = QPushButton('Is there enough time to \nengage in the activity?', self)
         time_in_activity_button.setGeometry((size.width()) / 4 - 260, 620, 220, 50)
+        time_in_activity_button.setStyleSheet("background-color: #00C5CD")
         self.connect(time_in_activity_button, SIGNAL('clicked()'), self.time_in_activity)
 
-        proceed_next_activity_button = QPushButton('Proceed to next \nfixed activity', self)
-        proceed_next_activity_button.setGeometry((size.width()) / 4 + 40, 620, 220, 50)
-        #self.connect(proceed_next_activity_button, SIGNAL('clicked()'), self.proceed_next_activity)
+#        proceed_next_activity_button = QPushButton('Proceed to next \nfixed activity', self)
+#        proceed_next_activity_button.setGeometry((size.width()) / 4 + 40, 620, 220, 50)
+#        proceed_next_activity_button.setStyleSheet("background-color: #FFFDD0")
+#        #self.connect(proceed_next_activity_button, SIGNAL('clicked()'), self.proceed_next_activity)
 
         mode_choice_next_activity_button = QPushButton('Mode Choice to the \nnext fixed activity', self)
         mode_choice_next_activity_button.setGeometry((size.width()) / 4 + 40, 700, 220, 50)
+        mode_choice_next_activity_button.setStyleSheet("background-color: #00C5CD")
         self.connect(mode_choice_next_activity_button, SIGNAL('clicked()'), self.mode_choice_next_activity)
 
         hov_button = QPushButton('Is the mode of \nthe trip HOV?', self)
         hov_button.setGeometry((size.width()) / 4 - 250, 790, 200, 50)
+        hov_button.setStyleSheet("background-color: #00C5CD")
         self.connect(hov_button, SIGNAL('clicked()'), self.hov)
 
         activity_pursued_2_button = QPushButton('Can activity be pursued \njointly (Spatial and Temporal \nfeasibility) with Household \nmembers?', self)
         activity_pursued_2_button.setGeometry((size.width()) / 4 + 50, 780, 200, 70)
+        activity_pursued_2_button.setStyleSheet("background-color: #00C5CD")
         self.connect(activity_pursued_2_button, SIGNAL('clicked()'), self.activity_pursued_2)
 
         check_if_join_activity_button = QPushButton('For each available household \nmember, check to see if \nhe/she will join the activity?', self)
         check_if_join_activity_button.setGeometry((size.width()) / 4 + 350, 780, 200, 70)
+        check_if_join_activity_button.setStyleSheet("background-color: #00C5CD")
         self.connect(check_if_join_activity_button, SIGNAL('clicked()'), self.check_if_join_activity)
 
-        activity_non_hhold_button = QPushButton('Joint Activity with \nNon-household member', self)
-        activity_non_hhold_button.setGeometry((size.width()) / 4 + 50, 880, 200, 50)
-        #self.connect(activity_non_hhold_button, SIGNAL('clicked()'), self.activity_non_hhold)
-
-        activity_hhold_button = QPushButton('Joint Activity with \nhousehold member', self)
-        activity_hhold_button.setGeometry((size.width()) / 4 + 350, 880, 200, 50)
-        #self.connect(activity_hhold_button, SIGNAL('clicked()'), self.activity_hhold)
+#        activity_non_hhold_button = QPushButton('Joint Activity with \nNon-household member', self)
+#        activity_non_hhold_button.setGeometry((size.width()) / 4 + 50, 880, 200, 50)
+#        activity_non_hhold_button.setStyleSheet("background-color: #FFFDD0")
+#        #self.connect(activity_non_hhold_button, SIGNAL('clicked()'), self.activity_non_hhold)
+#
+#        activity_hhold_button = QPushButton('Joint Activity with \nhousehold member', self)
+#        activity_hhold_button.setGeometry((size.width()) / 4 + 350, 880, 200, 50)
+#        activity_hhold_button.setStyleSheet("background-color: #FFFDD0")
+#        #self.connect(activity_hhold_button, SIGNAL('clicked()'), self.activity_hhold)
 
         sov_hov_button = QPushButton('If mode is SOV or HOV Driver identify \nvehicle (for only trips where household \nmember is the driver) ', self)
         sov_hov_button.setGeometry((size.width()) / 2 - 150, 990, 300, 50)
+        sov_hov_button.setStyleSheet("background-color: #00C5CD")
         self.connect(sov_hov_button, SIGNAL('clicked()'), self.sov_hov)
 
-        activity_travel_pattern_button = QPushButton('Activity-travel patterns for \nall individuals within the time-slice', self)
-        activity_travel_pattern_button.setGeometry((size.width()) / 2 - 150, 1070, 300, 50)
-        #self.connect(activity_travel_pattern_button, SIGNAL('clicked()'), self.activity_travel_pattern)
         
         Dummy  = QPushButton('', self)
         Dummy.setGeometry(0, size.height() - 4, 1140, 2)
@@ -381,6 +394,17 @@ class Activity_Travel_Pattern_Simulator(QWidget):
         arrow.setBrush(QColor("black"))
         arrow.drawPolygon(QPoint(point.x(),point.y()),QPoint(point.x() - 4,point.y() - 13), QPoint(point.x() + 4,point.y() - 13))
 
+        arrow.setBrush(QColor("#F0F0F0"))
+        arrow.drawRoundedRect(widgetwidth/2 - 100, 40, 200, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/2 - 150, 1070, 300, 50, 15.0, 15.0)
+        
+        arrow.drawRoundedRect(widgetwidth*3/4 - 100, 120, 200, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/4 - 110, 120, 220, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/4 + 40, 620, 220, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/4 + 50, 880, 200, 50, 15.0, 15.0)
+        arrow.drawRoundedRect(widgetwidth/4 + 350, 880, 200, 50, 15.0, 15.0)       
+
+        
         arrow.end()
 
         # Drawing text
@@ -432,8 +456,6 @@ class Activity_Travel_Pattern_Simulator(QWidget):
         point.setY(865)
         text.drawText(point, "Yes")
 
-        
-
         point.setX(widgetwidth / 4 - 210)  
         point.setY(890)
         text.drawText(point, "SOV \53")
@@ -442,8 +464,29 @@ class Activity_Travel_Pattern_Simulator(QWidget):
         point.setY(905)
         text.drawText(point, "Other Modes")
 
+        temp = QRect(widgetwidth/2 - 100, 40, 200, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Within a time slice')
 
-        text.begin(self)
+        temp = QRect(widgetwidth/2 - 150, 1070, 300, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Activity-travel patterns for \nall individuals within the time-slice')
+        
+        temp = QRect(widgetwidth*3/4 - 100, 120, 200, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Children with after school \ndependent activities')
+
+        temp = QRect(widgetwidth/4 - 110, 120, 220, 50)
+        text.drawText(temp, Qt.AlignCenter, 'All other individuals')
+        
+        temp = QRect(widgetwidth/4 + 40, 620, 220, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Proceed to next \nfixed activity')
+        
+        temp = QRect(widgetwidth/4 + 50, 880, 200, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Joint Activity with \nNon-household member')
+        
+        temp = QRect(widgetwidth/4 + 350, 880, 200, 50)
+        text.drawText(temp, Qt.AlignCenter, 'Joint Activity with \nhousehold member')        
+
+        
+        text.end()
 
         
 
