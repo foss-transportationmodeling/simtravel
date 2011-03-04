8045c2a7a34758d9350eb75c1a8251eb0122995f
karthik.charan <karthik.charan@8e946292-11aa-11df-992a-f3fa5211fe9f>
2011-03-04 23:15:54 +0000

    code added to allow for parallelizing the openamos run by dividing the inputs into multiple chunks

--- src/trunk/openamos/core/openamos_run.py
+++ src/trunk/openamos/core/openamos_run.py
@@ -1,9 +1,10 @@
 import sys
+import multiprocessing
 
 from openamos.core.run.simulation_manager_cursor import SimulationManager
 from openamos.core.errors import ArgumentsError
 
-def run_long_term_choices():
+def run():
     """
     Runs the OpenAMOS program to simulate the activity-travel choices as 
     indicated by the models and their specifications in the config file.
@@ -13,24 +14,62 @@ def run_long_term_choices():
     """
     args = sys.argv[1:]
 
-    print args
-
-    if len(args) <> 1:
-        raise ArgumentsError, """The module accepts """\
-            """only one argument which is the location of the configuration """\
-            """file. e.g. /home/config.xml (linux machine) """\
-            """or c:/testproject/config.xml (windows machine)"""
-
     
-    simulationManagerObject = SimulationManager(fileLoc = args[0])
-    simulationManagerObject.setup_databaseConnection()
-    simulationManagerObject.setup_cacheDatabase('w')
-    simulationManagerObject.setup_location_information()
-    simulationManagerObject.setup_tod_skims()
+    if len(args) < 1 or len(args) > 2:
+        raise ArgumentsError, """The module accepts """\
+            """only two arguments which are the location of the configuration """\
+            """ file . e.g. /home/config.xml (linux machine) """\
+            """or c:/testproject/config.xml (windows machine) and """\
+            """and the number of parts to run in parallel """\
+
+    fileLoc = args[0]
+
+    if len(args) == 1:
+        simulationManagerObject = SimulationManager(fileLoc = fileLoc)
+        queryBrowser = simulationManagerObject.setup_databaseConnection()
+        simulationManagerObject.setup_cacheDatabase()
+        simulationManagerObject.setup_tod_skims(queryBrowser)
+        simulationManagerObject.setup_location_information(queryBrowser)
+        simulationManagerObject.close_database_connection(queryBrowser)
+        simulationManagerObject.parse_config()
+        simulationManagerObject.clean_database_tables()
+        simulationManagerObject.run_components()
+        simulationManagerObject.close_cache_connection()
+    else:
+        numParts = int(args[1])
+        simulationManagerObject = SimulationManager(fileLoc = fileLoc)
+        simulationManagerObject.divide_database(numParts)
+        #simulationManagerObject.parse_config()
+        #simulationManagerObject.clean_database_tables()
+        
+        """
+        #Multiprocessing
+        pool = multiprocessing.Pool()
+        partsList = range(numParts)
+        print partsList
+        argsParallel = [(fileLoc, i+1) for i in partsList]
+        print argsParallel
+        pool.map(run_components_in_parallel, argsParallel)
+        """
+        simulationManagerObject.collate_results(numParts)
+        
+def run_components_in_parallel(args):
+    print args
+    
+    fileLoc = args[0]
+    partId = args[1]
+    
+    simulationManagerObject = SimulationManager(fileLoc = fileLoc)
+    simulationManagerObject.setup_cacheDatabase(partId)
+    queryBrowser = simulationManagerObject.setup_databaseConnection()
+    simulationManagerObject.setup_tod_skims(queryBrowser)
+    simulationManagerObject.setup_location_information(queryBrowser)
+    simulationManagerObject.close_database_connection(queryBrowser)    
     simulationManagerObject.parse_config()
-    simulationManagerObject.clean_database_tables()
-    simulationManagerObject.run_components()
-    simulationManagerObject.close_connections()
+    simulationManagerObject.clean_database_tables(partId)
+    simulationManagerObject.run_components(partId)
+    simulationManagerObject.close_cache_connection()
+
 
 if __name__ == "__main__":
-    sys.exit(run_long_term_choices())
+    sys.exit(run())
