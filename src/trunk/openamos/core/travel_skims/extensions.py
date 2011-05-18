7895aa50febdb08a74f8f9eedcc633b78b7b762e
bapat.namrata <bapat.namrata@8e946292-11aa-11df-992a-f3fa5211fe9f>
2011-05-18 17:09:41 +0000

    Take nodes as an argument. Take the file path as argument

--- src/trunk/openamos/core/travel_skims/extensions.py
+++ src/trunk/openamos/core/travel_skims/extensions.py
@@ -15,8 +15,10 @@ class Extensions(object):
     Input: 
     """
 
-    def __init__(self, offset):
+    def __init__(self, offset, nodes):
         self.offset = offset
+        self.nodes = nodes
+        print self.offset, self.nodes
         print 'Testing extension'
 
 
@@ -57,7 +59,7 @@ class Extensions(object):
         arrayexample.set_array(self.offset)
         t2 = time.time()
         print '\t\tTime taken %s'%(t2-t1)
-
+        
         return nodes, edges
 
         
@@ -86,12 +88,11 @@ class Extensions(object):
         Output:
         Carray
         """
-
         new_carr = arrayexample.new_floatArray(arr_len)
         i = 0
 
         for each in numpy_arr:
-            arrayexample.floatArray_setitem(new_carr, i, each)
+            arrayexample.floatArray_setitem(new_carr, i, float(each))
             i = i + 1
 
         return new_carr
@@ -188,6 +189,12 @@ class Extensions(object):
         """
         arrayexample.delete_array()
         print 'Dynamic arrays deleted'
+        
+    def set_string(self, file_path, flag):
+        """
+        """
+        arrayexample.set_file(file_path, 1, flag)
+        arrayexample.print_string()
 
 
 
@@ -199,20 +206,40 @@ import unittest
 class TestExtensions(unittest.TestCase):
     def setUp(self):
         self.numarr = 'numarr'
-        self.off = 1
+        self.offset = 1
+        self.nodes = 0
 
     def testEx(self):
-        ext_obj = Extensions(self.off)
+        ext_obj = Extensions(self.offset, self.nodes)
         
-        n, e = ext_obj.create_graph()
-        #print 'n: %s e: %s'%(n, e)
+        node_path = "/home/namrata/Documents/swig/arr_sample/temp_data_copy.csv"
+        graph_path = "/home/namrata/Documents/swig/arr_sample/skim1.dat"
+        flag = 1
+        ext_obj.set_string(node_path, flag)
+        flag = 0
+        ext_obj.set_string(graph_path, flag)
 
+        n, e = ext_obj.create_graph()
+        print 'n: %s e: %s'%(n, e)
+        #arrayexample.print_org_array(self.offset)
         #for small sample
         """
         origin = array([1,2,3,4,5,6,7,8,9,10])
         destination = array([10,9,8,7,6,5,4,3,2,1])
         arr_len = origin.size
         tt = zeros(arr_len)
+        
+        origin = zeros(10)
+        destination = zeros(10)
+        arr_len = origin.size
+        tt = zeros(arr_len)
+        i = 0
+        j = 10
+        offset = 1
+        for i in range(0,10):
+            origin[i] = i + offset
+            destination[i] = j
+            j = j - 1
         """
         
         #for full data
@@ -238,14 +265,7 @@ class TestExtensions(unittest.TestCase):
             origin[i] = i+offset
             destination[i] = j
             j = j -1
-        """
-        # 2.5 thousand
-        j = 1000
-        for i in range(2000, 2100):
-            origin[i] = i/10
-            destination[i] = j
-            j = j -1
-        """
+        
         #3rd thousand
         j = 2000
         for i in range(2000, 3000):
@@ -302,18 +322,27 @@ class TestExtensions(unittest.TestCase):
             destination[i] = j
             j = j -1
         
-
+        
         print 'travel times'
         print '\t\t\t\t', time.time()
         new_tt = ext_obj.get_travel_times(origin, destination, tt, arr_len)
         print '\t\t\t\t', time.time()
                 
         print 'array size ', new_tt.size
-        #for i in range(0, 100):
-        #    print new_tt[i]
+        """
+        #for each in new_tt:
+        #    print each
+        org = ext_obj.numpy_to_carray(origin, arr_len)
+        dest = ext_obj.numpy_to_carray(destination, arr_len)
+        tt_arr = ext_obj.numpy_to_carray(new_tt, arr_len)
+        arrayexample.print_tt_array(org, dest, tt_arr, arr_len)
+        """
+        for i in range(0, 100):
+            print new_tt[i]
         
         ext_obj.delete_graph()
-
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
