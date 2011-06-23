c46cdcb7cb6eb46a013b3545333060590e8d8eb9
Dae.You@asu.edu <Dae.You@asu.edu@8e946292-11aa-11df-992a-f3fa5211fe9f>
2011-06-23 17:39:33 +0000

    Upgrade tree view

--- src/trunk/openamos/gui/misc/basic_widgets.py
+++ src/trunk/openamos/gui/misc/basic_widgets.py
@@ -53,3 +53,10 @@ class ComboBoxFolder(QComboBox):
             if location == self.itemText(i):
                 return i
         return None
+    
+class TreeWidgetItem(QTreeWidgetItem):
+    def __init__(self, parent, sets, values):
+        super(TreeWidgetItem, self).__init__(parent)
+#        self.attribute = attr
+        self.sets = sets
+        self.values = values
