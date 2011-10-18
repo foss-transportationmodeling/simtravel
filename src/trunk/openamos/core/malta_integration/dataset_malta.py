56f4cd596abdbc90f858a6ad8bdd0092eb846621
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2011-10-18 19:21:36 +0000

    modified the layout of the new locations table based off of the new model estimatations ...

--- src/trunk/openamos/core/malta_integration/dataset_malta.py
+++ src/trunk/openamos/core/malta_integration/dataset_malta.py
@@ -13,10 +13,13 @@ class DB(object):
 
 
 	self.tableCols = {'travel_skims':['origin', 'destination', 'tt'], 
-			  'locations':['locationid', 'retail_employment', 'public_employment', 
-				       'office_employment', 'industrial_employment', 'other_employment',
-				       'residential_population', 'total_area', 'multi_family_dwelling', 
-				       'single_family_dwelling', 'institutional_population'],
+			  'locations':['locationid', 'retail_employment', 'office_employment', 'public_employment',
+				       'industrial_employment', 'other_employment',
+				       'retail_employment_density', 'office_employment_density', 'public_employment_density', 
+				       'other_employment_density',
+				       'total_area', 'low_income', 'lowest_income', 'high_income',
+				       'institutional_population', 'groupquarter_households',
+				       'residential_households'],
 			  'households_vehicles_count_r':['houseid', 'vehcount'],
 			  'vehicles_r':['houseid', 'vehid', 'vehtype'],
 			  'schedule_r':['houseid', 'personid', 'activitytype', 'locationid', 'starttime',
@@ -88,7 +91,15 @@ class DB(object):
 			'public_employment_ind':int32, 'retail_employment_ind':int32, 
 			'office_employment_ind':int32, 'industrial_employment_ind':int32, 'other_employment_ind':int32, 
 			'total_area':float32, 'residential_population':int32, 'single_family_dwelling':int32, 
-			'institutional_population':int32, 'multi_family_dwelling':int32, 'vehcount':int32, 
+			'institutional_population':int32, 'multi_family_dwelling':int32, 
+
+			'retail_employment_density':float32, 'public_employment_density':float32, 
+			'office_employment_density':float32, 'industrial_employment_density':float32, 'other_employment_density':float32,
+		       	'total_area':float32, 'lowest_income':int32, 'low_income':int32, 'high_income':int32,
+			'institutional_popultion':int32, 'groupquarter_households':int32,
+			'residential_households':int32,
+
+			'vehcount':int32, 
 			'vehtype':int32, 'scheduleid':int32, 'activitytype':int32, 'duration':int32, 
 			'dependentpersonid':int64, 'ih_history':int32, 'discretionary_history':int32, 
 			'maintenance_history':int32, 'fixed_history':int32, 
@@ -122,6 +133,7 @@ class DB(object):
 	return self.tableCols[tableName]
 
     def returnColId(self, tableName, colName):
+	#print 'colId', colName
 	return self.tableCols[tableName].index(colName)
 
     def returnTable(self, tableName, id_var, colNames, fileLoc):
