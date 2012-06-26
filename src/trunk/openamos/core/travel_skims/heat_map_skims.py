b837c83c267c6517e6690c7bc17a046415cf89dd
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2012-06-26 16:36:34 +0000

    The heat map skims doesn't plot the scatter plots anymore, only the deviation is returned. This change was made because it was causing some runtime errors during remote sessions.

--- src/trunk/openamos/core/travel_skims/heat_map_skims.py
+++ src/trunk/openamos/core/travel_skims/heat_map_skims.py
@@ -86,11 +86,11 @@ class PlotHeatMap(object):
 	plt.show()
 
     def createHeatMapForXY(self, xName, xLoc, yName, yLoc, fName=None, fLoc=None):
-	plt.subplots_adjust(hspace=0.1)
+	#plt.subplots_adjust(hspace=0.1)
 
-	subPlotNum = 111
+	#subPlotNum = 111
 		
-	plt.subplot(subPlotNum)
+	#plt.subplot(subPlotNum)
 
 	x = self.load_file(xLoc)
 
@@ -105,26 +105,26 @@ class PlotHeatMap(object):
 	#print 'First 5 x values - ', x[:5,-1]
 	#print 'First 5 y values - ', y[:5,-1]
 
-	plt.hexbin(x[:,-1],y[:,-1], cmap=cm.jet, bins=100)
-	plt.axis([x_min, x_max, y_min, y_max])
+	#plt.hexbin(x[:,-1],y[:,-1], cmap=cm.jet, bins=100)
+	#plt.axis([x_min, x_max, y_min, y_max])
 
-	plt.title("%s(Y) Vs %s(X)" %(yName, xName))
+	#plt.title("%s(Y) Vs %s(X)" %(yName, xName))
 
-	cb = plt.colorbar()
-	cb.set_label('Count')
+	#cb = plt.colorbar()
+	#cb.set_label('Count')
 
 	#plt.show()
-	plt.savefig('%s%s%s' %(fLoc, os.path.sep, fName), format='png')
+	#plt.savefig('%s%s%s' %(fLoc, os.path.sep, fName), format='png')
 
         return (np.abs(x[:,-1] - y[:,-1])).sum()
 
 
     def createHeatMapForIncompleteXY(self, xName, xLoc, yName, yLoc, fName=None, fLoc=None):
-	plt.subplots_adjust(hspace=0.1)
+	#plt.subplots_adjust(hspace=0.1)
 
-	subPlotNum = 111
+	#subPlotNum = 111
 		
-	plt.subplot(subPlotNum)
+	#plt.subplot(subPlotNum)
 
 	x = self.load_file(xLoc)
 	y = self.load_file(yLoc)
@@ -152,17 +152,17 @@ class PlotHeatMap(object):
 	print 'First 5 x values - ', x[:105], x_min, x_max
 	print 'First 5 y values - ', y[:105], y_min, y_max
 
-	plt.hexbin(x,y, cmap=cm.jet)
-	plt.axis([x_min, x_max, y_min, y_max])
+	#plt.hexbin(x,y, cmap=cm.jet)
+	#plt.axis([x_min, x_max, y_min, y_max])
 
-	plt.title("%s(Y) Vs %s(X)" %(yName, xName))
-
-	
-	cb = plt.colorbar()
-	cb.set_label('Count')
+	#plt.title("%s(Y) Vs %s(X)" %(yName, xName))
 
 	
-	plt.savefig('%s%s%s' %(fLoc, os.path.sep, fName), format='png')
+	#cb = plt.colorbar()
+	#cb.set_label('Count')
+
+	
+	#plt.savefig('%s%s%s' %(fLoc, os.path.sep, fName), format='png')
 
         return (np.abs(x - y)).sum()
 
@@ -201,9 +201,9 @@ if __name__ == "__main__":
 		       locOld='/home/karthik/simtravel/test/mag_zone_dynamic/skimOutput/dynamic/startSkims/')
 	
     #obj12.calculate_deviation()
-    dev = obj12.createHeatMapForXY('iter1', '/home/karthik/simtravel/test/mag_zone_dynamic/iteration_1/skim3.dat',
-    			     'iter2', '/home/karthik/simtravel/test/mag_zone_dynamic/iteration_2/skim3.dat',
-			     fLoc = '/home/karthik/simtravel/test/mag_zone_dynamic/iteration_2/',
+    dev = obj12.createHeatMapForXY('iter1', '/home/karthik/simtravel/projects/mag_zone_dynamic/year_2009/iteration_1/skim21.dat',
+    			     'iter2', '/home/karthik/simtravel/projects/mag_zone_dynamic/year_2009/iteration_2/skim21.dat',
+			     fLoc = '/home/karthik/simtravel/projects/mag_zone_dynamic/year_2009/iteration_2/',
 			     fName = 'skim3_iter2_vs_iter1.dat')
     print dev	
 
