773ec1c6889c18fdf98583d44db1f1238fdf0d08
karthik.charan@gmail.com <karthik.charan@gmail.com@8e946292-11aa-11df-992a-f3fa5211fe9f>
2013-07-11 18:56:20 +0000

    simplified the implementation of the random number generation

--- src/trunk/openamos/core/models/abstract_random_distribution_model.py
+++ src/trunk/openamos/core/models/abstract_random_distribution_model.py
@@ -1,66 +1,28 @@
-from numpy import zeros, array
+from numpy import zeros, array, iinfo, int32
 from numpy.random.mtrand import RandomState
 
-class RandomDistribution(object):
+class RandomDistribution(RandomState):
     def __init__(self, seed):
-        #RandomState.__init__(self, seed)
-        self.seed = seed
+        RandomState.__init__(self, seed)
+	self.seed = seed
 
     def return_normal_variables(self, location, scale, size):
         if scale == 0:
             return zeros(size) + location
 
-	norm_vars = []
-	
-	
-	#print 'Here is the size - ', size
-	for count in range(size[0]):
-	    i = self.seed[count]
-
-	    try:
-	    	locI = location[count]
-	    except TypeError, e:
-		locI = location
-
-	    randStObj = RandomState(i)
-	    norm_vars.append(randStObj.normal(loc=locI, scale=scale))
-
-	norm_vars = array(norm_vars)
-	norm_vars.shape = (size[0],1)
-
-	#print 'Returning shape', norm_vars.shape
-
-
-        return norm_vars
-
+	rand_norm_vars = self.normal(loc=location, scale=scale, size=size)
+	return rand_norm_vars
         
     def return_random_variables(self, size=None):
-	#print 'this is size in random variables - ', size
-	#print 'this is seed - ', self.seed
-        if size == None:
-	    randStObj = RandomState(self.seed)
-            return randStObj.random_sample()
-    	rand_vars = []
-	for count in range(size):
-	    i = self.seed[count]
+	if size == None:
+	    return self.random_sample()
 
-	    randStObj = RandomState(i)
-	    rand_vars.append(randStObj.random_sample())
-
-	rand_vars = array(rand_vars)
-
-        return rand_vars
+	rand_vars = self.random_sample(size)
+	return rand_vars
 
     def return_random_integers(self, low, high=None, size=None):
-	rand_vars = []
-	for count in range(size[0]):
-	    i = self.seed[count]
-
-	    randStObj = RandomState(i)
-	    rand_vars.append(randStObj.random_intergers(low, high))
-
-	rand_vars = array(rand_vars)
-	return rand_vars
+	rand_int_vars = self.random_integers(low, high, size)
+	return rand_int_vars
 
     def return_half_normal_variables(self, location, scale, size=1):
 	norm_vars = self.return_normal_variables(location, scale, size)
@@ -69,29 +31,7 @@ class RandomDistribution(object):
 	return half_norm_vars
 
     def shuffle_sequence(self, sequence):
-	
-	randStObj = RandomState(self.seed)
-	return randStObj.shuffle(sequence)
-
-    def return_uniform(self, low=0.0, high=1.0, size=1):
-	unif_vars = []
-	for count in range(size[0]):
-	    i = self.seed[count]
-
-	    randStObj = RandomState(i)
-	    unif_vars.append(randStObj.uniform(low, high))
-
-	    randStObj = RandomState(i)
-	    print 'Repeat 1 ', randStObj.uniform(low, high)
-	
-	    randStObj = RandomState(i)
-	    print 'Repeat 2 ', randStObj.uniform(low, high)
-	    raw_input('repeat uniform')
-	unif_vars = array(unif_vars)
-	return unif_vars
-
-
-
+	return self.shuffle(sequence)
 
 import unittest
 
